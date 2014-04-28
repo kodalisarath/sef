e2f68377230e3df3122404cc42fffd19bca2ac1c
ummadisudhakar <ummadi.sudhakar.reddy@gmail.com>
2014-04-28 14:48:08 +0530

    config serveice related POJO

---
+++ sef-core/src/main/java/com/ericsson/raso/sef/core/config/Section.java
@@ -0,0 +1,53 @@
+package com.ericsson.raso.sef.core.config;
+
+import java.util.List;  
+import java.util.ArrayList;  
+import javax.xml.bind.annotation.XmlAttribute;  
+import javax.xml.bind.annotation.XmlElement;  
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAccessType;
+
+
+@XmlRootElement(name="section")
+
+public class Section {
+	
+	private String id;  
+	private String description;  
+	private ArrayList<Property>property = new ArrayList<Property>() ; 
+	public Section() {}  
+	public Section(String id, String description, ArrayList<Property> property) {  
+	    super();  
+	    this.id = id;  
+	    this.description = description;  
+	    this.property = property;    
+	}  
+	
+	@XmlAttribute
+	public String getId() {  
+	    return id;  
+	}  
+	public void setId(String id) {  
+	    this.id = id;  
+	}
+	
+	@XmlAttribute
+	public String getDescription() {  
+	    return description;  
+	}  
+	public void setDescription(String description) {  
+	    this.description = description;  
+	}  
+	
+	
+	public ArrayList<Property> getProperty() {  
+	    return property;  
+	}  
+	
+	@XmlElement(name="property")
+	public void setProperty(ArrayList<Property> property) {  
+	    this.property = property;  
+	}  
+
+}
