95ef012b07113b05bf5797939011c402f1a7231c
Jayasankar <l.jaya.sankar@ericsson.com>
2014-04-29 16:29:12 +0700

    Refactored the implementation with interface to facilitate OSGI exposure

---
+++ sef-core/src/main/java/com/ericsson/raso/sef/core/CloudAwareCluster.java
@@ -0,0 +1,25 @@
+package com.ericsson.raso.sef.core;
+
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.locks.Lock;
+
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IAtomicLong;
+
+public interface CloudAwareCluster {
+
+	public void start();
+	public void stop();
+	
+	public <K, V> Map<K, V> getMap(String name);
+	
+	public ExecutorService getDistributedService(String name) ;
+	
+	public IAtomicLong getAtomicCounter(String name);
+	
+	public Lock getLock(String key);
+	
+	public HazelcastInstance getInstance();
+
+}
