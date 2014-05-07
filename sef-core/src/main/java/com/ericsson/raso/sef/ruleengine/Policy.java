56a0ac5c13c80c75dd803e939f703a6aa44fd62b
rasocac <sathya.narayanan.s@ericsson.com>
2014-05-07 08:53:59 +0800

    Implemented a new class Policy to keep in line with Rule... A Policy can allow transformtive actions when Rules execute successful.

---
+++ sef-core/src/main/java/com/ericsson/raso/sef/ruleengine/Policy.java
@@ -0,0 +1,67 @@
+package com.ericsson.raso.sef.ruleengine;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Policy implements Action, Serializable {
+	
+	private static final long serialVersionUID = -1780481144582884377L;
+	
+	private String name = null;
+	private Rule rule = null;
+	private List<TransformUnit> transforms = new ArrayList<TransformUnit>();
+
+	public Policy() { }
+	
+	public Policy(String name) {
+		this.name = name;
+	}
+	
+	public void addTransformAction(TransformUnit transform) {
+		this.transforms.add(transform);
+	}
+	
+	public void removeTransformAction(TransformUnit transform) {
+		this.transforms.remove(transform);
+	}
+	
+	
+
+	@Override
+	public boolean execute() {
+		if (this.rule.execute()) {
+			for (TransformUnit transform: this.transforms) {
+				try {
+					transform.execute();
+				} catch (TransformFailedException e) {
+					// TODO logger on the error...
+					return false;
+				}
+			}
+		}
+
+		return true;
+	}
+
+	public Rule getRule() {
+		return rule;
+	}
+
+	public void setRule(Rule rule) {
+		this.rule = rule;
+	}
+
+	public List<TransformUnit> getTransforms() {
+		return transforms;
+	}
+
+	public void setTransforms(List<TransformUnit> transforms) {
+		this.transforms = transforms;
+	}
+	
+	
+
+	
+}
