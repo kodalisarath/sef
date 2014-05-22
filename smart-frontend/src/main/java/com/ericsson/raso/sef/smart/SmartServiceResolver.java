a2ee4a70156d631d13dce9f6606ad28043d534c7
Jayasankar <l.jaya.sankar@ericsson.com>
2014-05-22 11:01:55 +0700

    Fixed compilation issue

--- smart-frontend/src/main/java/com/ericsson/raso/sef/smart/SmartServiceResolver.java
+++ smart-frontend/src/main/java/com/ericsson/raso/sef/smart/SmartServiceResolver.java
@@ -4,10 +4,6 @@ import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
-import com.ericsson.raso.sef.core.config.IConfig;
-import com.ericsson.sef.bes.api.subscriber.SubscriberManagement;
-import com.ericsson.sef.bes.api.subscription.SubscriptionManagement;
-
 public class SmartServiceResolver implements ApplicationContextAware {
 
 	private static ApplicationContext context;
@@ -18,14 +14,6 @@ public class SmartServiceResolver implements ApplicationContextAware {
 		SmartServiceResolver.context = arg0;
 	}
 
-	
-	public static SubscriberManagement getSubscriberManagement() {
-		return SmartServiceResolver.context.getBean(SubscriberManagement.class);
-	}
-
-	public static SubscriptionManagement getSubscriptionManagement() {
-		return SmartServiceResolver.context.getBean(SubscriptionManagement.class);
-	}
 	
 	public static <T> T getBean(Class<T> type) {
 		return context.getBean(type);
