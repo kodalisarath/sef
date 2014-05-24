55b532a0c359ba6a31604a430a3a1dc4889d9f48
Jayasankar <l.jaya.sankar@ericsson.com>
2014-05-24 10:11:16 +0700

    constants introduced to be used by all components

---
+++ sef-core/src/main/java/com/ericsson/raso/sef/core/Constants.java
@@ -0,0 +1,8 @@
+package com.ericsson.raso.sef.core;
+
+public class Constants {
+
+	public static final String TRANSPARENT_META1 = "TRANSPARENTMETA1";
+	public static final String TRANSPARENT_META2 = "TRANSPARENTMETA2";
+	public static final String TRANSPARENT_META3 = "TRANSPARENTMETA3";
+}
