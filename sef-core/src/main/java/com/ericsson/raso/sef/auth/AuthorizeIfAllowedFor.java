8f119052d29083d14a316c7d58ec17f6b336c836
Jayasankar <l.jaya.sankar@ericsson.com>
2014-05-24 10:09:50 +0700

    Changes to fit Java SE 7. @repeatable annotation suppressed

--- sef-core/src/main/java/com/ericsson/raso/sef/auth/AuthorizeIfAllowedFor.java
+++ sef-core/src/main/java/com/ericsson/raso/sef/auth/AuthorizeIfAllowedFor.java
@@ -1,7 +1,6 @@
 package com.ericsson.raso.sef.auth;
 
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -10,7 +9,7 @@ import com.ericsson.raso.sef.auth.permissions.AuthorizationPrinciple;
 
 @Target ({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
 @Retention (RetentionPolicy.RUNTIME)
-@Repeatable(Authorizations.class)
+//@Repeatable(Authorizations.class)
 public @interface AuthorizeIfAllowedFor {
 	public abstract AuthorizationPrinciple permission();
 	public abstract String referenceValueInIdentity();
