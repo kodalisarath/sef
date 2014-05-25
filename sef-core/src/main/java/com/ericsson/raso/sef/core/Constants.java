0ff10aa4659e921315de72778b4b1181a8f4e4a4
Jayasankar <l.jaya.sankar@ericsson.com>
2014-05-26 04:27:12 +0700

    constants

--- sef-core/src/main/java/com/ericsson/raso/sef/core/Constants.java
+++ sef-core/src/main/java/com/ericsson/raso/sef/core/Constants.java
@@ -2,7 +2,15 @@ package com.ericsson.raso.sef.core;
 
 public class Constants {
 
-	public static final String TRANSPARENT_META1 = "TRANSPARENTMETA1";
-	public static final String TRANSPARENT_META2 = "TRANSPARENTMETA2";
-	public static final String TRANSPARENT_META3 = "TRANSPARENTMETA3";
+	
+	//UCIP Specific - Constants used to pass ucip specific information to flow through from north bound interfaces
+	
+	public static final String TX_TYPE = "transcationType";
+	public static final String TX_CODE = "transcationCode";
+	public static final String TX_AMOUNT = "transactionAmount";
+	public static final String CHANNEL_NAME = "channelName";
+	public static final String EX_DATA1 = "externalData1";
+	public static final String EX_DATA2 = "externalData2";
+	public static final String EX_DATA3 = "externalData3";
+	
 }
