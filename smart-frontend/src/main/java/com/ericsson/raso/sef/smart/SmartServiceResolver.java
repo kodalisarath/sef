38fd802ea0036591242dfa1f9a1c0a40a1fec316
Jayasankar <l.jaya.sankar@ericsson.com>
2014-05-26 04:30:49 +0700

    Updated smart service resolver to get client proxy Added SMART specific constants

--- smart-frontend/src/main/java/com/ericsson/raso/sef/smart/SmartServiceResolver.java
+++ smart-frontend/src/main/java/com/ericsson/raso/sef/smart/SmartServiceResolver.java
@@ -4,6 +4,9 @@ import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
+import com.ericsson.sef.bes.api.subscriber.ISubscriberRequest;
+import com.ericsson.sef.bes.api.subscription.ISubscriptionRequest;
+
 public class SmartServiceResolver implements ApplicationContextAware {
 
 	private static ApplicationContext context;
@@ -18,4 +21,13 @@ public class SmartServiceResolver implements ApplicationContextAware {
 	public static <T> T getBean(Class<T> type) {
 		return context.getBean(type);
 	}
+	
+	public static ISubscriptionRequest getSubscriptionRequest() {
+		return context.getBean(ISubscriptionRequest.class);
+	}
+	
+	public static ISubscriberRequest getSubscriberRequest() {
+		return context.getBean(ISubscriberRequest.class);
+	}
+	
 }
---
+++ smart-frontend/src/main/java/com/ericsson/raso/sef/smart/commons/SmartConstants.java
@@ -0,0 +1,25 @@
+package com.ericsson.raso.sef.smart.commons;
+
+public class SmartConstants {
+	
+	private SmartConstants() {
+		
+	}
+	
+	
+	//SOAP Interface constants - used in interface unmarshaling
+	
+	public static final String EVENT_CLASS = "EventClass";
+	public static final String RESOURCE_ID = "resourceId";
+	public static final String PRODUCT_ID = "productId";
+	public static final String EXPIRY_DATE = "expiryDate";
+	public static final String VALIDITY_LEFT = "validityLeft";
+	public static final String EXPIRY_DAYS_OF_EXTENSION = "expiryDaysOfExtension";
+	public static final String EXPIRY_POLICY = "expiryPolicy";
+	public static final String CHARGED_USER_ID = "chargedUserId";
+	public static final String PURCHASE_TYPE = "purchaseType";
+	public static final String notificationMessage = "notificationMessage";
+	public static final String GRACE_PERIOD = "gracePeriod";
+	public static final String USECASE = "usecase";
+
+}
