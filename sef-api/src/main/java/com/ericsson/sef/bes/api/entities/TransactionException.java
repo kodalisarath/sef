687e4bfb09f2ddd620fe3aed74b26006df0a3242
n0th1n6 <nrafallo@gmail.com>
2014-05-27 12:19:21 +0800

    removed additional entry in smart-frontend/beans.xml

--- sef-api/src/main/java/com/ericsson/sef/bes/api/entities/TransactionException.java
+++ sef-api/src/main/java/com/ericsson/sef/bes/api/entities/TransactionException.java
@@ -1,11 +1,18 @@
 package com.ericsson.sef.bes.api.entities;
 
+import javax.xml.ws.WebFault;
 
+@WebFault
 public final class TransactionException extends Exception {
 	private static final long serialVersionUID = 2651520114941007027L;
 	
 	private String requestId = null;
-
+	
+	public TransactionException()
+	{
+		super();
+	}
+	
 	public TransactionException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
 		super(arg0, arg1, arg2, arg3);
 	}
