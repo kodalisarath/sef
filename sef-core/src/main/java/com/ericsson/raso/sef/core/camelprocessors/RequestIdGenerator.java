75fa8ea6344d121605533cad268dcb4f9001c276
n0th1n6 <nrafallo@gmail.com>
2014-05-28 13:34:05 +0800

    transaction engine engine fixes

--- sef-core/src/main/java/com/ericsson/raso/sef/core/camelprocessors/RequestIdGenerator.java
+++ sef-core/src/main/java/com/ericsson/raso/sef/core/camelprocessors/RequestIdGenerator.java
@@ -2,13 +2,18 @@ package com.ericsson.raso.sef.core.camelprocessors;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 
+import com.ericsson.raso.sef.core.RequestContext;
 import com.ericsson.raso.sef.core.RequestContextLocalStore;
 import com.ericsson.raso.sef.core.UniqueIdGenerator;
 
 public abstract class RequestIdGenerator implements Processor {
 
+	Logger logger = LoggerFactory.getLogger(RequestIdGenerator.class);
+	
 	@Override
 	public void process(Exchange arg0) throws Exception {
 		
@@ -16,7 +21,10 @@ public abstract class RequestIdGenerator implements Processor {
 		if(requestId == null) {
 			requestId = UniqueIdGenerator.generateId();
 			MDC.put("requestId", requestId);
-			RequestContextLocalStore.get().setRequestId(requestId);
+			logger.info("New request Id generated: " + requestId);
+			RequestContext ctx = new RequestContext();
+			ctx.setRequestId(requestId);
+			RequestContextLocalStore.put(ctx);
 		}
 	}
 	
--- transaction-engine/src/main/java/com/ericsson/raso/sef/bes/engine/transaction/processor/PurchaseProcessor.java
+++ transaction-engine/src/main/java/com/ericsson/raso/sef/bes/engine/transaction/processor/PurchaseProcessor.java
@@ -1,28 +1,48 @@
 package com.ericsson.raso.sef.bes.engine.transaction.processor;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.xml.ws.Holder;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.ericsson.raso.sef.bes.engine.transaction.ServiceResolver;
 import com.ericsson.raso.sef.bes.engine.transaction.TransactionManager;
+import com.ericsson.sef.bes.api.entities.Meta;
 
 public class PurchaseProcessor implements Processor{
 
+	Logger logger = LoggerFactory.getLogger(PurchaseProcessor.class);
+	
 	@SuppressWarnings("unchecked")
 	@Override
 	public void process(Exchange arg0) throws Exception {
-		Object[] objectArray = (Object[]) arg0.getIn().getBody(Object.class);
-		Holder<String> requestId = (Holder<String>)objectArray[0];
-		Holder<String> offerId = (Holder<String>)objectArray[1];
-		Holder<String> subscriberId=(Holder<String>)objectArray[2];
-		Holder<Map> metas=(Holder<Map>)objectArray[3];
-		Holder<Boolean> override=(Holder<Boolean>)objectArray[4];
+		Object[] objectArray = arg0.getIn().getBody(Object[].class);
+		String requestId = (String) objectArray[0];
+		String offerId = (String)objectArray[1];
+		String subscriberId=(String)objectArray[2];
+		logger.debug("requestID: " + requestId);
+		logger.debug("offerId: " + offerId);
+		logger.debug("subscriberId: " + subscriberId);
+		boolean override=(boolean)objectArray[3];
+		List<Meta> metas= (List<Meta>)objectArray[4];
 		TransactionManager transactionManager=ServiceResolver.getTransactionManager();
-		transactionManager.purchase(requestId.value, offerId.value, subscriberId.value, override.value, metas.value);
+		transactionManager.purchase(requestId, offerId, subscriberId, override, convertToList(metas));
+		
+	}
+	/*Method to convert a list to a map*/
+	private Map<String,Object> convertToList(List<Meta> metas){
+		Map<String,Object> metaMap = new HashMap<String,Object>();
+		for(Meta meta: metas){
+			metaMap.put(meta.getKey(), meta.getValue());
+		}
+		return metaMap;
 		
 	}
 
