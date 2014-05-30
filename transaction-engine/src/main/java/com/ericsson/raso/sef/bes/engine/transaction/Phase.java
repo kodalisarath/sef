fef5c5e95f7bc33c8dbfd22d72da380ee13b3f5f
Jayasankar <l.jaya.sankar@ericsson.com>
2014-05-30 09:27:17 +0700

    Implemented serializable

--- transaction-engine/src/main/java/com/ericsson/raso/sef/bes/engine/transaction/Phase.java
+++ transaction-engine/src/main/java/com/ericsson/raso/sef/bes/engine/transaction/Phase.java
@@ -1,6 +1,8 @@
 package com.ericsson.raso.sef.bes.engine.transaction;
 
-public enum Phase {
+import java.io.Serializable;
+
+public enum Phase implements Serializable {
 	TX_PHASE_CHARGING,
 	TX_PHASE_PREP_FULFILLMENT,
 	TX_PHASE_FULFILLMENT,
