f0cea0a36707fa0bdcec2393380ca450e695130d
Jayasankar <l.jaya.sankar@ericsson.com>
2014-05-30 09:47:08 +0700

    Added utility method to retrieve next phase

--- transaction-engine/src/main/java/com/ericsson/raso/sef/bes/engine/transaction/Phase.java
+++ transaction-engine/src/main/java/com/ericsson/raso/sef/bes/engine/transaction/Phase.java
@@ -7,6 +7,22 @@ public enum Phase implements Serializable {
 	TX_PHASE_PREP_FULFILLMENT,
 	TX_PHASE_FULFILLMENT,
 	TX_PHASE_SCHEDULE,
-	TX_PHASE_PERSISTENCE,  
+	TX_PHASE_PERSISTENCE,
+	TX_PHASE_ALL_COMPLETED;
 	
+	public Phase getNextPhase() {
+		
+		switch(this) {
+		case TX_PHASE_CHARGING:
+			return TX_PHASE_PREP_FULFILLMENT;
+		case TX_PHASE_PREP_FULFILLMENT:
+			return TX_PHASE_FULFILLMENT;
+		case TX_PHASE_FULFILLMENT:
+			return TX_PHASE_SCHEDULE;
+		case TX_PHASE_PERSISTENCE:
+			return TX_PHASE_ALL_COMPLETED;
+		default:
+			return TX_PHASE_CHARGING;
+		}
+	}
 }
