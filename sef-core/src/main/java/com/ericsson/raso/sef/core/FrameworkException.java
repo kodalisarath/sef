85489af27e152c424f207a5e5b63d6f24e68302c
Jayasankar <l.jaya.sankar@ericsson.com>
2014-06-02 13:09:04 +0700

    FrameworkException changes in local are not committed to repo.. untrack-addtogit-commmit

--- sef-core/src/main/java/com/ericsson/raso/sef/core/FrameworkException.java
+++ sef-core/src/main/java/com/ericsson/raso/sef/core/FrameworkException.java
@@ -3,33 +3,55 @@ package com.ericsson.raso.sef.core;
 public class FrameworkException extends Exception {
 
 	private static final long serialVersionUID = 5793034755089703055L;
-
-	public FrameworkException() {
-		super();
-		// TODO Auto-generated constructor stub
+	
+	public static final StatusCode globalError = new ResponseCode(500, "Internal Server Error");
+	
+	private String component = "core";
+	private StatusCode statusCode = globalError;
+	
+	public FrameworkException(String component, StatusCode code) {
+		this(code.getMessage());
+		this.component = component;
+		this.statusCode = code;
+	}
+	
+	public FrameworkException(String component, StatusCode code, Throwable e) {
+		this(code.getMessage(), e);
+		this.component = component;
+		this.statusCode = code;
 	}
 
 	public FrameworkException(String arg0, Throwable arg1, boolean arg2,
 			boolean arg3) {
 		super(arg0, arg1, arg2, arg3);
-		// TODO Auto-generated constructor stub
 	}
 
 	public FrameworkException(String arg0, Throwable arg1) {
 		super(arg0, arg1);
-		// TODO Auto-generated constructor stub
 	}
 
 	public FrameworkException(String arg0) {
 		super(arg0);
-		// TODO Auto-generated constructor stub
 	}
 
 	public FrameworkException(Throwable arg0) {
 		super(arg0);
-		// TODO Auto-generated constructor stub
 	}
-	
-	
+
+	public String getComponent() {
+		return component;
+	}
+
+	public void setComponent(String component) {
+		this.component = component;
+	}
+
+	public StatusCode getStatusCode() {
+		return statusCode;
+	}
+
+	public void setStatusCode(StatusCode statusCode) {
+		this.statusCode = statusCode;
+	}
 
 }
