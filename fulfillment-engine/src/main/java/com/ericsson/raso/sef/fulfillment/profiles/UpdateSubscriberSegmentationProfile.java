4821b55f69dfdb352092402b2bd81535dc5bed01
Jayasankar <l.jaya.sankar@ericsson.com>
2014-06-05 03:59:20 +0700

    UpdateSubscriberSegmentation profile

--- fulfillment-engine/src/main/java/com/ericsson/raso/sef/fulfillment/profiles/UpdateSubscriberSegmentationProfile.java
+++ fulfillment-engine/src/main/java/com/ericsson/raso/sef/fulfillment/profiles/UpdateSubscriberSegmentationProfile.java
@@ -4,13 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.ericsson.raso.sef.client.air.command.UpdateSubscriberSegmentationCmd;
 import com.ericsson.raso.sef.client.air.request.ServiceOffering;
+import com.ericsson.raso.sef.client.air.request.UpdateSubscriberSegmentRequest;
+import com.ericsson.raso.sef.core.ResponseCode;
+import com.ericsson.raso.sef.core.SmException;
 import com.ericsson.raso.sef.fulfillment.commons.FulfillmentException;
 import com.ericsson.sef.bes.api.entities.Product;
 
 public class UpdateSubscriberSegmentationProfile extends BlockingFulfillment<Product>{
 
 	private static final long serialVersionUID = -4012237785041809835L;
+	private static final Logger logger = LoggerFactory.getLogger(UpdateSubscriberSegmentationProfile.class);
 	
 	List<ServiceOffering> serviceOfferings = new ArrayList<ServiceOffering>();
 	
@@ -22,15 +30,31 @@ public class UpdateSubscriberSegmentationProfile extends BlockingFulfillment<Pro
 	@Override
 	public List<Product> fulfill(Product e, Map<String, String> map)
 			throws FulfillmentException {
-		// TODO Auto-generated method stub
-		return null;
+		logger.info("Entering fulfilment for UpdateSubscriberSegmentation");
+		
+		List<Product> products = new ArrayList<Product>();
+		if (map == null || map.isEmpty())
+			throw new FulfillmentException("ffe", new ResponseCode(1001, "runtime parameters 'metas' missing in request!!"));
+		
+		UpdateSubscriberSegmentRequest request = new UpdateSubscriberSegmentRequest();
+		String msisdn = map.get("msisdn");
+		request.setSubscriberNumber(msisdn);
+		request.setServiceOfferings(serviceOfferings);
+		try {
+			new UpdateSubscriberSegmentationCmd(request).execute();
+		} catch (SmException e1) {
+			logger.error("Refill execution failed!!", e1);
+			throw new FulfillmentException(e1.getComponent(), new ResponseCode(e1.getStatusCode().getCode(), e1.getStatusCode().getMessage()));
+		} 
+		return products;
 	}
 
 	@Override
 	public List<Product> prepare(Product e, Map<String, String> map)
 			throws FulfillmentException {
-		// TODO Auto-generated method stub
-		return null;
+		logger.info("Entering prepare for UpdateSubscriberSegmentation");
+		List<Product> products = new ArrayList<Product>();
+		return products;
 	}
 
 	@Override
