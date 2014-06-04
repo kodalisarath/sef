1b5e2de1a4df603e4a76208edcba82718ad094a3
vinay548 <vinayreddy548@gmail.com>
2014-06-04 20:44:48 +0530

    Updated the classes.

--- fulfillment-engine/src/main/java/com/ericsson/raso/sef/fulfillment/profiles/CreateSubscriberProfile.java
+++ fulfillment-engine/src/main/java/com/ericsson/raso/sef/fulfillment/profiles/CreateSubscriberProfile.java
@@ -3,6 +3,15 @@ package com.ericsson.raso.sef.fulfillment.profiles;
 import java.util.List;
 import java.util.Map;
 
+import com.ericsson.raso.sef.client.af.command.AddDnsCommand;
+import com.ericsson.raso.sef.client.af.request.AddDnsRequest;
+import com.ericsson.raso.sef.client.air.command.DeleteSubscriberCommand;
+import com.ericsson.raso.sef.client.air.command.InstallSubscriberCommand;
+import com.ericsson.raso.sef.client.air.request.DeleteSubscriberRequest;
+import com.ericsson.raso.sef.client.air.request.InstallSubscriberRequest;
+import com.ericsson.raso.sef.core.RequestContext;
+import com.ericsson.raso.sef.core.SefCoreServiceResolver;
+import com.ericsson.raso.sef.core.SmException;
 import com.ericsson.sef.bes.api.entities.Product;
 
 public class CreateSubscriberProfile extends BlockingFulfillment<Product> {
@@ -93,6 +102,64 @@ public class CreateSubscriberProfile extends BlockingFulfillment<Product> {
 	@Override
 	public List<Product> fulfill(Product e, Map<String, String> map) {
 		// TODO Auto-generated method stub
+		
+		CreateSubscriberProfile createSubscriberProfile= new CreateSubscriberProfile(originOperatorId);
+		
+		AddDnsRequest dnsRequest = new AddDnsRequest();
+		dnsRequest.setMsisdn(map.get("msisdn"));		
+		
+		
+		RequestContext requestContext = new RequestContext();
+		
+		String sdpId = requestContext.getInterfaceName();
+		dnsRequest.setSdpId(sdpId);
+		
+		try {
+			new AddDnsCommand(dnsRequest).execute();
+		} catch (SmException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+				
+		String defaultServiceClass = SefCoreServiceResolver.getConfigService().getValue("GLOBAL", "defaultServiceClass");
+		
+		//String defaultServiceClass = FederationProfileContext.getProperty(FederationProfileContext.DEFAULT_SERVICE_CLASS);
+		
+		InstallSubscriberRequest request = new InstallSubscriberRequest();
+		if(createSubscriberProfile.getPromotionPlanId() != null) {
+			request.setPromotionPlanId(createSubscriberProfile.getPromotionPlanId());
+		}
+		
+		if(createSubscriberProfile.getLanguageIDNew() !=null) {
+			request.setLanguageIDNew(createSubscriberProfile.getLanguageIDNew());
+		}
+		
+		if(createSubscriberProfile.getMessageCapabilityFlag() != null) {
+			request.setMessageCapabilityFlag(createSubscriberProfile.getMessageCapabilityFlag());
+		}
+		if(request.getPamInformationList() != null) {
+			request.setPamInformationList(createSubscriberProfile.getPamInformationList().getPamInfolist());
+		}
+		
+		request.setServiceClassNew(Integer.valueOf(defaultServiceClass));
+		
+		if(request.getTemporaryBlockedFlag() !=null) {
+			request.setTemporaryBlockedFlag(createSubscriberProfile.getTemporaryBlockedFlag());
+		}
+		
+		if(request.getUssdEndOfCallNotificationID() !=null) {
+			request.setUssdEndOfCallNotificationID(createSubscriberProfile.getUssdEndOfCallNotificationID());
+		}
+		request.setSubscriberNumber(map.get("msisdn"));
+		InstallSubscriberCommand instlCommand = new InstallSubscriberCommand(request);
+		try {
+			instlCommand.execute();
+		} catch (SmException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
 		return null;
 	}
 
@@ -111,107 +178,24 @@ public class CreateSubscriberProfile extends BlockingFulfillment<Product> {
 	@Override
 	public List<Product> revert(Product e, Map<String, String> map) {
 		// TODO Auto-generated method stub
+		CreateSubscriberProfile createSubscriberProfile= new CreateSubscriberProfile(originOperatorId);
+		DeleteSubscriberRequest request = new DeleteSubscriberRequest();
+		request.setSubscriberNumber(map.get("msisdn"));
+		request.setOriginOperatorId(createSubscriberProfile.getOriginOperatorId());
+		try {
+			new DeleteSubscriberCommand(request).execute();
+		} catch (SmException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
 		return null;
 	}
 
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result
-				+ ((languageIDNew == null) ? 0 : languageIDNew.hashCode());
-		result = prime
-				* result
-				+ ((messageCapabilityFlag == null) ? 0 : messageCapabilityFlag
-						.hashCode());
-		result = prime
-				* result
-				+ ((originOperatorId == null) ? 0 : originOperatorId.hashCode());
-		result = prime
-				* result
-				+ ((pamInformationList == null) ? 0 : pamInformationList
-						.hashCode());
-		result = prime * result
-				+ ((promotionPlanId == null) ? 0 : promotionPlanId.hashCode());
-		result = prime * result
-				+ ((serviceClassNew == null) ? 0 : serviceClassNew.hashCode());
-		result = prime
-				* result
-				+ ((temporaryBlockedFlag == null) ? 0 : temporaryBlockedFlag
-						.hashCode());
-		result = prime
-				* result
-				+ ((ussdEndOfCallNotificationID == null) ? 0
-						: ussdEndOfCallNotificationID.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		CreateSubscriberProfile other = (CreateSubscriberProfile) obj;
-		if (languageIDNew == null) {
-			if (other.languageIDNew != null)
-				return false;
-		} else if (!languageIDNew.equals(other.languageIDNew))
-			return false;
-		if (messageCapabilityFlag == null) {
-			if (other.messageCapabilityFlag != null)
-				return false;
-		} else if (!messageCapabilityFlag.equals(other.messageCapabilityFlag))
-			return false;
-		if (originOperatorId == null) {
-			if (other.originOperatorId != null)
-				return false;
-		} else if (!originOperatorId.equals(other.originOperatorId))
-			return false;
-		if (pamInformationList == null) {
-			if (other.pamInformationList != null)
-				return false;
-		} else if (!pamInformationList.equals(other.pamInformationList))
-			return false;
-		if (promotionPlanId == null) {
-			if (other.promotionPlanId != null)
-				return false;
-		} else if (!promotionPlanId.equals(other.promotionPlanId))
-			return false;
-		if (serviceClassNew == null) {
-			if (other.serviceClassNew != null)
-				return false;
-		} else if (!serviceClassNew.equals(other.serviceClassNew))
-			return false;
-		if (temporaryBlockedFlag == null) {
-			if (other.temporaryBlockedFlag != null)
-				return false;
-		} else if (!temporaryBlockedFlag.equals(other.temporaryBlockedFlag))
-			return false;
-		if (ussdEndOfCallNotificationID == null) {
-			if (other.ussdEndOfCallNotificationID != null)
-				return false;
-		} else if (!ussdEndOfCallNotificationID
-				.equals(other.ussdEndOfCallNotificationID))
-			return false;
-		return true;
-	}
-
-	@Override
 	public String toString() {
-		return "CreateSubscriberProfile [messageCapabilityFlag="
-				+ messageCapabilityFlag + ", promotionPlanId="
-				+ promotionPlanId + ", serviceClassNew=" + serviceClassNew
-				+ ", languageIDNew=" + languageIDNew
-				+ ", temporaryBlockedFlag=" + temporaryBlockedFlag
-				+ ", ussdEndOfCallNotificationID="
-				+ ussdEndOfCallNotificationID + ", originOperatorId="
-				+ originOperatorId + ", pamInformationList="
-				+ pamInformationList + "]";
+		// TODO Auto-generated method stub
+		return null;
 	}
-
-	
 
 }
--- fulfillment-engine/src/main/java/com/ericsson/raso/sef/fulfillment/profiles/DnsUpdateProfile.java
+++ fulfillment-engine/src/main/java/com/ericsson/raso/sef/fulfillment/profiles/DnsUpdateProfile.java
@@ -3,6 +3,12 @@ package com.ericsson.raso.sef.fulfillment.profiles;
 import java.util.List;
 import java.util.Map;
 
+import com.ericsson.raso.sef.client.af.command.AddDnsCommand;
+import com.ericsson.raso.sef.client.af.command.DeleteDnsCommand;
+import com.ericsson.raso.sef.client.af.request.AddDnsRequest;
+import com.ericsson.raso.sef.client.af.request.DeleteDnsRequest;
+import com.ericsson.raso.sef.core.RequestContextLocalStore;
+import com.ericsson.raso.sef.core.SmException;
 import com.ericsson.sef.bes.api.entities.Product;
 
 public class DnsUpdateProfile extends BlockingFulfillment<com.ericsson.sef.bes.api.entities.Product> {
@@ -65,12 +71,34 @@ public class DnsUpdateProfile extends BlockingFulfillment<com.ericsson.sef.bes.a
 	@Override
 	public List<Product> fulfill(Product e, Map<String, String> map) {
 		// TODO Auto-generated method stub
+		DnsUpdateProfile dnsUpdateProfile=new DnsUpdateProfile(zname);
+		AddDnsRequest dnsRequest = new AddDnsRequest();
+		
+		dnsRequest.setMsisdn(map.get("msisdn"));
+		dnsRequest.setDclass(dnsUpdateProfile.getDclass());
+		dnsRequest.setDtype(dnsUpdateProfile.getDtype());
+		dnsRequest.setRdata(dnsUpdateProfile.getRdata());
+		dnsRequest.setTtl(dnsUpdateProfile.getTtl());
+		dnsRequest.setZname(dnsUpdateProfile.getZname());
+		
+		String sdpId= (String) RequestContextLocalStore.get().getInProcess().get("sdpId");
+		//requestContext.get("sdpId");
+		dnsRequest.setSdpId(sdpId);
+		
+		try {
+			new AddDnsCommand(dnsRequest).execute();
+		} catch (SmException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		 
 		return null;
 	}
 
 	@Override
 	public List<Product> prepare(Product e, Map<String, String> map) {
 		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
@@ -83,54 +111,31 @@ public class DnsUpdateProfile extends BlockingFulfillment<com.ericsson.sef.bes.a
 	@Override
 	public List<Product> revert(Product e, Map<String, String> map) {
 		// TODO Auto-generated method stub
+		
+		DnsUpdateProfile dnsUpdateProfile=new DnsUpdateProfile(zname);
+		DeleteDnsRequest deleteDnsRequest = new DeleteDnsRequest();
+		
+		deleteDnsRequest.setMsisdn(map.get("msisdn"));
+		deleteDnsRequest.setDclass(dnsUpdateProfile.getDclass());
+		deleteDnsRequest.setDtype(dnsUpdateProfile.getDtype());
+		deleteDnsRequest.setSiteId(null);
+		deleteDnsRequest.setTtl(dnsUpdateProfile.getTtl());
+		deleteDnsRequest.setZname(dnsUpdateProfile.getZname());
+		
+		try {
+			new DeleteDnsCommand(deleteDnsRequest).execute();
+		} catch (SmException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
 		return null;
 	}
 
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + dclass;
-		result = prime * result + dtype;
-		result = prime * result + ((rdata == null) ? 0 : rdata.hashCode());
-		result = prime * result + ttl;
-		result = prime * result + ((zname == null) ? 0 : zname.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		DnsUpdateProfile other = (DnsUpdateProfile) obj;
-		if (dclass != other.dclass)
-			return false;
-		if (dtype != other.dtype)
-			return false;
-		if (rdata == null) {
-			if (other.rdata != null)
-				return false;
-		} else if (!rdata.equals(other.rdata))
-			return false;
-		if (ttl != other.ttl)
-			return false;
-		if (zname == null) {
-			if (other.zname != null)
-				return false;
-		} else if (!zname.equals(other.zname))
-			return false;
-		return true;
-	}
-
-	@Override
 	public String toString() {
-		return "DnsUpdateProfile [zname=" + zname + ", rdata=" + rdata
-				+ ", dtype=" + dtype + ", dclass=" + dclass + ", ttl=" + ttl
-				+ "]";
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 }
