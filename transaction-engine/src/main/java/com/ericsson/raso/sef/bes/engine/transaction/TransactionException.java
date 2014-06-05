ac2226ca83adb6dd6a9d2cc0c5ccef664b153b64
Jayasankar <l.jaya.sankar@ericsson.com>
2014-06-06 05:04:40 +0700

    update subscriber use case correction

--- transaction-engine/src/main/java/com/ericsson/raso/sef/bes/engine/transaction/TransactionException.java
+++ transaction-engine/src/main/java/com/ericsson/raso/sef/bes/engine/transaction/TransactionException.java
@@ -1,12 +1,22 @@
 package com.ericsson.raso.sef.bes.engine.transaction;
 
 import com.ericsson.raso.sef.core.FrameworkException;
+import com.ericsson.raso.sef.core.StatusCode;
 
 public final class TransactionException extends FrameworkException {
 	private static final long serialVersionUID = 2651520114941007027L;
 	
 	private String requestId = null;
 
+	
+	public TransactionException(String component, StatusCode code) {
+		super(component, code);
+	}
+	
+	public TransactionException(String component, StatusCode code, Throwable e) {
+		super(component, code, e);
+	}
+	
 	public TransactionException(String requestId, String arg0) {
 		super(arg0);
 		this.requestId = requestId;
