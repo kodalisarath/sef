4c1f919149f3a070f32ec252316d8292e7a632ad
rasocac <sathya.narayanan.s@ericsson.com>
2014-06-05 12:05:31 +0800

    Improved logic to ensure no null handling on the compareTo()

--- sef-product-catalog/src/main/java/com/ericsson/raso/sef/bes/prodcat/entities/AtomicProduct.java
+++ sef-product-catalog/src/main/java/com/ericsson/raso/sef/bes/prodcat/entities/AtomicProduct.java
@@ -7,19 +7,19 @@ import java.util.TreeMap;
 import com.ericsson.raso.sef.bes.prodcat.SubscriptionLifeCycleEvent;
 
 public final class AtomicProduct extends Product implements Serializable, Comparable<AtomicProduct> {
-	private static final long serialVersionUID = 7193202158721069988L;
+	private static final long							serialVersionUID	= 7193202158721069988L;
 
-	private Resource resource = null;
-	private AbstractQuotaCharacteristic quota = null;
-	private AbstractTimeCharacteristic validity = null;
-	private Map<String, Object> metas = null;
-	private Map<SubscriptionLifeCycleEvent, Boolean> suppressFulfilment = null;
-	private boolean resetQuotaOnRenewal = false;
-	
+	private Resource									resource			= null;
+	private AbstractQuotaCharacteristic					quota				= null;
+	private AbstractTimeCharacteristic					validity			= null;
+	private Map<String, Object>							metas				= null;
+	private Map<SubscriptionLifeCycleEvent, Boolean>	suppressFulfilment	= null;
+	private boolean										resetQuotaOnRenewal	= false;
+
 	public AtomicProduct(String name) {
 		super(name);
 	}
-	
+
 	public Resource getResource() {
 		return resource;
 	}
@@ -31,21 +31,21 @@ public final class AtomicProduct extends Product implements Serializable, Compar
 	public void setMeta(String metaName, Object value) {
 		if (this.metas == null)
 			this.metas = new TreeMap<String, Object>();
-		
+
 		this.metas.put(metaName, value);
 	}
-	
+
 	public void removeMeta(String metaName) {
 		if (this.metas != null)
 			this.metas.remove(metaName);
 	}
-	
+
 	public Object getMeta(String metaName) {
 		if (this.metas != null)
 			return this.metas.get(metaName);
 		return null;
 	}
-	
+
 	public Map<String, Object> getMetas() {
 		return this.metas;
 	}
@@ -83,30 +83,29 @@ public final class AtomicProduct extends Product implements Serializable, Compar
 	}
 
 	public boolean isSame(AtomicProduct other) {
-		
+
 		if (other == null)
 			return false;
-		
+
 		if (this.resource == null)
 			return false;
-		
+
 		if (other.resource == null)
 			return false;
-		
+
 		if (!this.resource.equals(other.resource))
 			return false;
-		
+
 		if (!this.quota.equals(other.quota))
 			return false;
-		
+
 		if (!this.validity.equals(other.validity))
 			return false;
-		
-		
-		
+
+
 		return true;
 	}
-	
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -118,61 +117,68 @@ public final class AtomicProduct extends Product implements Serializable, Compar
 		result = prime * result + ((validity == null) ? 0 : validity.hashCode());
 		return result;
 	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
 		if (obj == null)
 			return false;
-		
+
 		if (this == obj)
 			return true;
-		
+
 		if (!super.equals(obj))
 			return false;
-		
+
 		if (!(obj instanceof AtomicProduct))
 			return false;
-		
+
 		AtomicProduct other = (AtomicProduct) obj;
 		if (quota == null) {
 			if (other.quota != null)
 				return false;
 		} else if (!quota.equals(other.quota))
 			return false;
-		
+
 		if (resetQuotaOnRenewal != other.resetQuotaOnRenewal)
 			return false;
-		
+
 		if (resource == null) {
 			if (other.resource != null)
 				return false;
 		} else if (!resource.equals(other.resource))
 			return false;
-		
+
 		if (suppressFulfilment == null) {
 			if (other.suppressFulfilment != null)
 				return false;
 		} else if (!suppressFulfilment.equals(other.suppressFulfilment))
 			return false;
-		
+
 		if (validity == null) {
 			if (other.validity != null)
 				return false;
 		} else if (!validity.equals(other.validity))
 			return false;
-		
+
 		return true;
 	}
 
 	@Override
 	public int compareTo(AtomicProduct o) {
-		if (this.quota.getDefinedQuota() > o.quota.getDefinedQuota())
-			return 1;
-		else
-			return -1;
+		if (this.quota != null && o.quota != null) {
+			if (this.quota.getDefinedQuota() > o.quota.getDefinedQuota())
+				return 1;
+			else
+				return -1;
+		} else if (this.validity != null && o.validity != null) {
+			if (this.validity.getExpiryTimeInMillis() > o.validity.getExpiryTimeInMillis())
+				return 1;
+			else
+				return -1;
+		} else {
+			return this.getName().compareTo(getName());
+		}
 	}
 
-	
-	
-	
+
 }
