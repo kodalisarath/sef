97068c6e63f27383f360e8a7c12b8e99e27f4b3c
Jayasankar <l.jaya.sankar@ericsson.com>
2014-06-05 19:46:58 +0700

    Bug Fix: org.apache.cxf.interceptor.Fault: java.util.ArrayList cannot be cast to java.util.Map

--- transaction-engine/src/main/java/com/ericsson/raso/sef/bes/engine/transaction/processor/UpdateSubscriberProcessor.java
+++ transaction-engine/src/main/java/com/ericsson/raso/sef/bes/engine/transaction/processor/UpdateSubscriberProcessor.java
@@ -1,5 +1,7 @@
 package com.ericsson.raso.sef.bes.engine.transaction.processor;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.camel.Exchange;
@@ -7,6 +9,7 @@ import org.apache.camel.Processor;
 
 import com.ericsson.raso.sef.bes.engine.transaction.ServiceResolver;
 import com.ericsson.raso.sef.bes.engine.transaction.TransactionManager;
+import com.ericsson.sef.bes.api.entities.Meta;
 
 public class UpdateSubscriberProcessor implements Processor{
 
@@ -15,9 +18,21 @@ public class UpdateSubscriberProcessor implements Processor{
 		Object[] objectArray = (Object[]) arg0.getIn().getBody(Object[].class);
 		String requestId = (String) objectArray[0];
 		String subscriberId = (String) objectArray[1];
-		Map metas=(Map) objectArray[2];
+		List<Meta> metas=(List<Meta>) objectArray[2];
 		TransactionManager transactionManager = ServiceResolver.getTransactionManager();
-		transactionManager.updateSubscriber(requestId, subscriberId, metas);
+		transactionManager.updateSubscriber(requestId, subscriberId, getMap(metas));
+	}
+	
+	private Map<String,String> getMap(List<Meta> metas) {
+		
+		Map<String, String> map = new HashMap<String, String>();
+		if(metas != null) {
+			for(Meta meta: metas) {
+				map.put(meta.getKey(), meta.getValue());
+			}
+		}
+		return null;
+		
 	}
 
 }
