380fd2fe5492c09db872d8c44bea6a1207c85498
ankittiwari0105 <ankit.tiwari@vekomy.com>
2014-06-09 23:42:32 +0700

    BucketRetrieveReadROP committed .

--- smart-frontend/src/main/java/com/ericsson/raso/sef/smart/processor/BucketRetrieveReadROP.java
+++ smart-frontend/src/main/java/com/ericsson/raso/sef/smart/processor/BucketRetrieveReadROP.java
@@ -15,7 +15,9 @@ import com.ericsson.raso.sef.core.SmException;
 import com.ericsson.raso.sef.smart.SmartServiceResolver;
 import com.ericsson.raso.sef.smart.subscriber.response.SubscriberInfo;
 import com.ericsson.raso.sef.smart.subscriber.response.SubscriberResponseStore;
-import com.ericsson.raso.sef.smart.usecase.BucketCreateOrWriteRopRequest;
+import com.ericsson.raso.sef.smart.usecase.BucketRetrieveReadROPRequest;
+import com.ericsson.raso.sef.smart.usecase.Usecase;
+import com.ericsson.sef.bes.api.entities.Meta;
 import com.ericsson.sef.bes.api.subscriber.ISubscriberRequest;
 import com.hazelcast.core.ISemaphore;
 import com.nsn.ossbss.charge_once.wsdl.entity.tis.xsd._1.CommandResponseData;
@@ -24,31 +26,59 @@ import com.nsn.ossbss.charge_once.wsdl.entity.tis.xsd._1.Operation;
 import com.nsn.ossbss.charge_once.wsdl.entity.tis.xsd._1.OperationResult;
 import com.nsn.ossbss.charge_once.wsdl.entity.tis.xsd._1.TransactionResult;
 
-
-
-
 public class BucketRetrieveReadROP implements Processor {
-	private static final Logger logger = LoggerFactory.getLogger(BucketCreateOrWriteRop.class);
+	private static final Logger logger = LoggerFactory
+			.getLogger(BucketRetrieveReadROP.class);
+	
 	@Override
 	public void process(Exchange exchange) throws Exception {
-		BucketCreateOrWriteRopRequest request = (BucketCreateOrWriteRopRequest) exchange.getIn().getBody();
+		// get the method signature of web service [Standard]
+		logger.debug("Getting exchange body....");
+		BucketRetrieveReadROPRequest bucketRetrieveReadROPRequest = (BucketRetrieveReadROPRequest) exchange
+				.getIn().getBody();
+		logger.debug("Got it!");
 		
-			List<com.ericsson.sef.bes.api.entities.Meta> metas = new ArrayList<com.ericsson.sef.bes.api.entities.Meta>();
-			metas.add(new com.ericsson.sef.bes.api.entities.Meta("category", request.getCategory()));
-			metas.add(new com.ericsson.sef.bes.api.entities.Meta("key", String.valueOf(request.getKey())));
-			metas.add(new com.ericsson.sef.bes.api.entities.Meta("bValidFrom", request.getbValidFrom()));
-			metas.add(new com.ericsson.sef.bes.api.entities.Meta("bInvalidFrom", request.getbInvalidFrom()));
-			metas.add(new com.ericsson.sef.bes.api.entities.Meta("OnPeakAccountID_FU", request.getOnPeakAccountID_FU()));
-			metas.add(new com.ericsson.sef.bes.api.entities.Meta("messageId", String.valueOf(request.getMessageId())));
-			String requestId = RequestContextLocalStore.get().getRequestId();
-			updateSubscriber(requestId, request.getCustomerId(), metas);
-			
-			CommandResponseData responseData = createResponse(request.getUsecase().getOperation(), request.getUsecase().getModifier(),request.isTransactional());
-			exchange.getOut().setBody(responseData);
+		// functional service logic to be ported here [Porting work]
+		List<Meta> metas = new ArrayList<Meta>();
+		metas.add(new Meta("command", bucketRetrieveReadROPRequest.getCommand()));
+		metas.add(new Meta("customerid", bucketRetrieveReadROPRequest.getCustomerId()));
+		Usecase smartUsecase = bucketRetrieveReadROPRequest.getUsecase();
+		String operationName = smartUsecase.getOperation();
+		String operationModifier = smartUsecase.getModifier();
 		
+		String requestId = RequestContextLocalStore.get().getRequestId();
+		SubscriberInfo subscriberinfo = readSubscriber(requestId, bucketRetrieveReadROPRequest.getCustomerId(),metas);
+		// need error code response for EntireReadSubscriber
+				if(subscriberinfo == null) {
+					/*throw new SmException(new ResponseCode(-111,
+							"13423#EntireRead Entity - Customer with primary key Keyname:PK,CustomerId: " + readRequest.getCustomerId()
+									+ " does not exist"));*/
+					throw new SmException(new ResponseCode(504, "13423#Bucket Retrieve Read ROP Entity - Customer with primary key Keyname:PK,CustomerId: " + bucketRetrieveReadROPRequest.getCustomerId()
+							+ " does not exist"));
+				}
+		CommandResponseData responseData = createResponse(bucketRetrieveReadROPRequest.getUsecase().getOperation(), bucketRetrieveReadROPRequest.getUsecase().getModifier(),bucketRetrieveReadROPRequest.isTransactional());
+		exchange.getOut().setBody(responseData);
+				
 	}
 	
-	
+	private SubscriberInfo readSubscriber(String requestId, String customerId, List<Meta> metas) {
+		logger.info("Invoking bucket retrieve read rop subscriber on tx-engine subscriber interface");
+		ISubscriberRequest iSubscriberRequest = SmartServiceResolver.getSubscriberRequest();
+		SubscriberInfo subInfo = new SubscriberInfo();
+		SubscriberResponseStore.put(requestId, subInfo);
+		iSubscriberRequest.readSubscriber(requestId, customerId, metas);
+		ISemaphore semaphore = SefCoreServiceResolver.getCloudAwareCluster().getSemaphore(requestId);
+		try {
+		semaphore.init(0);
+		semaphore.acquire();
+		} catch(InterruptedException e) {
+			
+		}
+		logger.info("Check if response received for Read subscriber");
+		SubscriberInfo subscriberInfo = (SubscriberInfo) SubscriberResponseStore.remove(requestId);
+		return subscriberInfo;
+	}
+
 	private CommandResponseData createResponse(String operationName, String modifier, boolean isTransactional) {
 		logger.info("Invoking create Response");
 		CommandResponseData responseData = new CommandResponseData();
@@ -71,31 +101,5 @@ public class BucketRetrieveReadROP implements Processor {
 			
 		return responseData;
 	}
-	private SubscriberInfo updateSubscriber(String requestId, String customer_id,List<com.ericsson.sef.bes.api.entities.Meta> metas) throws SmException {
-		logger.info("Invoking create subscriber on tx-engine subscriber interface");
-		ISubscriberRequest iSubscriberRequest = SmartServiceResolver.getSubscriberRequest();
-		SubscriberInfo subInfo = new SubscriberInfo();
-		SubscriberResponseStore.put(requestId, subInfo);
-		iSubscriberRequest.updateSubscriber(requestId, customer_id,metas);
-		
-		ISemaphore semaphore = SefCoreServiceResolver.getCloudAwareCluster().getSemaphore(requestId);
-		
-		try {
-		semaphore.init(0);
-		semaphore.acquire();
-		} catch(InterruptedException e) {
-			
-		}
-		
-		logger.info("Check if response received for create subscriber");
-		
-		SubscriberInfo subscriberInfo = (SubscriberInfo) SubscriberResponseStore.remove(requestId);
-		if(subscriberInfo.getStatus().getCode() > 0){
-			ResponseCode resonseCode = new ResponseCode(subscriberInfo.getStatus().getCode(),subscriberInfo.getStatus().getDescription());
-			throw new SmException(resonseCode);
-			}
-		return subscriberInfo;
-	}
-
 	
 }
