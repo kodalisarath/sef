a9b694cdabf31594199459c0b547dffc683fc33f
n0th1n6 <nrafallo@gmail.com>
2014-06-10 12:56:10 +0800

    DeleteSubscriberProfile

--- cs-air/src/main/java/com/ericsson/raso/sef/client/air/response/DeleteSubscriberResponse.java
+++ cs-air/src/main/java/com/ericsson/raso/sef/client/air/response/DeleteSubscriberResponse.java
@@ -3,5 +3,19 @@ package com.ericsson.raso.sef.client.air.response;
 public class DeleteSubscriberResponse extends AbstractAirResponse {
 
 	private static final long serialVersionUID = 1L;
+	
+	private Boolean result;
 
+	public DeleteSubscriberResponse(String requestCorrelator) {
+		super();
+	}
+	
+	public Boolean getResult() {
+		return result;
+	}
+	
+	public void setResult(Boolean result) {
+		this.result = result;
+	}
+	
 }
--- fulfillment-engine/src/main/java/com/ericsson/raso/sef/fulfillment/profiles/DeleteSubscriberProfile.java
+++ fulfillment-engine/src/main/java/com/ericsson/raso/sef/fulfillment/profiles/DeleteSubscriberProfile.java
@@ -1,18 +1,49 @@
 package com.ericsson.raso.sef.fulfillment.profiles;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+
+import com.ericsson.raso.sef.client.af.command.AddDnsCommand;
+import com.ericsson.raso.sef.client.af.command.DeleteDnsCommand;
+import com.ericsson.raso.sef.client.af.request.AddDnsRequest;
+import com.ericsson.raso.sef.client.af.request.DeleteDnsRequest;
+import com.ericsson.raso.sef.client.air.command.DeleteSubscriberCommand;
+import com.ericsson.raso.sef.client.air.command.InstallSubscriberCommand;
+import com.ericsson.raso.sef.client.air.request.DeleteSubscriberRequest;
+import com.ericsson.raso.sef.client.air.request.InstallSubscriberRequest;
+import com.ericsson.raso.sef.core.RequestContextLocalStore;
+import com.ericsson.raso.sef.core.ResponseCode;
+import com.ericsson.raso.sef.core.SefCoreServiceResolver;
+import com.ericsson.raso.sef.core.SmException;
+import com.ericsson.raso.sef.fulfillment.commons.FulfillmentException;
 import com.ericsson.sef.bes.api.entities.Product;
+import com.hazelcast.logging.LoggerFactory;
 
 public class DeleteSubscriberProfile extends BlockingFulfillment<Product> {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -6336329642133594137L;
+	private static final Logger logger = org.slf4j.LoggerFactory.getLogger(DeleteSubscriberProfile.class);
+	
+	// CS-AF specific
+	private String zname;
+	private String rdata;
+	private int dtype;
+	private int dclass;
+	private int ttl;
 
-
+	// ACIP specific
+	private Boolean messageCapabilityFlag;
+	private Integer promotionPlanId;
+	private Integer serviceClassNew;
+	private Integer languageIDNew;
+	private Boolean temporaryBlockedFlag;
+	private Integer ussdEndOfCallNotificationID;
+	
+	private PamInformationList pamInformationList;
+	
 	public DeleteSubscriberProfile(String name) {
 		super(name);
 	}
@@ -30,21 +61,73 @@ public class DeleteSubscriberProfile extends BlockingFulfillment<Product> {
 	}
 
 	@Override
-	public List<Product> fulfill(Product e, Map<String, String> map) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Product> fulfill(Product e, Map<String, String> map) throws FulfillmentException {
+		logger.debug("Entering fulfill of DeleteSubscriberProfile...");
+		DeleteSubscriberProfile deleteSubscriberProfile = new DeleteSubscriberProfile(originOperatorId);
+	
+		List<Product> returned = new ArrayList<Product>();
+		
+		
+		// AF Install - function here
+		logger.debug("Preparing request pojo for CS-AF....");
+		DeleteDnsRequest dnsRequest = new DeleteDnsRequest();
+		dnsRequest.setMsisdn(map.get("SUBSCRIBER_ID"));
+		dnsRequest.setDclass(this.getDclass());
+		dnsRequest.setDtype(this.getDtype());
+		dnsRequest.setTtl(this.getTtl());
+		dnsRequest.setZname(this.getZname());
+		dnsRequest.setSiteId(null);
+		
+		logger.debug("Fetching the sdpId from Router");
+		String sdpId= (String) RequestContextLocalStore.get().getInProcess().get("sdpId");
+		
+		try {
+			logger.debug("Shall I fire the command to CS-AF DNS?");
+			new DeleteDnsCommand(dnsRequest).execute();
+			logger.debug("Deleted SUbscriber in CS-AF DNS");
+		} catch (SmException e1) {
+			logger.error("Failed AddDnsCommand execute" + e1.getMessage(), e);
+			throw new FulfillmentException(e1.getComponent(), new ResponseCode(e1.getStatusCode().getCode(), e1.getStatusCode().getMessage()));
+		}
+
+				
+		String defaultServiceClass = SefCoreServiceResolver.getConfigService().getValue("GLOBAL", "defaultServiceClass");
+		logger.debug("Configred defaultServiceClass: " + defaultServiceClass);
+		
+		DeleteSubscriberRequest request = new DeleteSubscriberRequest();
+		
+		//request.
+		if(deleteSubscriberProfile.getOriginOperatorId() != null) 
+			request.setOriginOperatorId(deleteSubscriberProfile.getOriginOperatorId());
+	
+		request.setSubscriberNumber(map.get("SUBSCRIBER_ID"));
+		DeleteSubscriberCommand deleteCommand = new DeleteSubscriberCommand(request);
+		logger.debug("Packed the request for execution..." + map.get("SUBSCRIBER_ID"));
+		
+		try {
+			deleteCommand.execute();
+			logger.debug("Subscriber installed in CS-AIR");
+		} catch (SmException e1) {
+			logger.error("Installing new subscriber failed!!", e1);
+			throw new FulfillmentException(e1.getComponent(), new ResponseCode(e1.getStatusCode().getCode(), e1.getStatusCode().getMessage()));
+		}
+		returned.add(e);
+		
+		return returned;	
 	}
 
 	@Override
 	public List<Product> prepare(Product e, Map<String, String> map) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Product> returned = new ArrayList<Product>();
+		returned.add(e);
+		return returned;
 	}
 
 	@Override
 	public List<Product> query(Product e, Map<String, String> map) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Product> returned = new ArrayList<Product>();
+		returned.add(e);
+		return returned;
 	}
 
 	@Override
@@ -84,6 +167,103 @@ public class DeleteSubscriberProfile extends BlockingFulfillment<Product> {
 	public String toString() {
 		return "DeleteSubscriberProfile [originOperatorId=" + originOperatorId
 				+ "]";
+	}
+
+	public String getZname() {
+		return zname;
+	}
+
+	public void setZname(String zname) {
+		this.zname = zname;
+	}
+
+	public String getRdata() {
+		return rdata;
+	}
+
+	public void setRdata(String rdata) {
+		this.rdata = rdata;
+	}
+
+	public int getDtype() {
+		return dtype;
+	}
+
+	public void setDtype(int dtype) {
+		this.dtype = dtype;
+	}
+
+	public int getDclass() {
+		return dclass;
+	}
+
+	public void setDclass(int dclass) {
+		this.dclass = dclass;
+	}
+
+	public int getTtl() {
+		return ttl;
+	}
+
+	public void setTtl(int ttl) {
+		this.ttl = ttl;
+	}
+
+	public Boolean getMessageCapabilityFlag() {
+		return messageCapabilityFlag;
+	}
+
+	public void setMessageCapabilityFlag(Boolean messageCapabilityFlag) {
+		this.messageCapabilityFlag = messageCapabilityFlag;
+	}
+
+	public Integer getPromotionPlanId() {
+		return promotionPlanId;
+	}
+
+	public void setPromotionPlanId(Integer promotionPlanId) {
+		this.promotionPlanId = promotionPlanId;
+	}
+
+	public Integer getServiceClassNew() {
+		return serviceClassNew;
+	}
+
+	public void setServiceClassNew(Integer serviceClassNew) {
+		this.serviceClassNew = serviceClassNew;
+	}
+
+	public Integer getLanguageIDNew() {
+		return languageIDNew;
+	}
+
+	public void setLanguageIDNew(Integer languageIDNew) {
+		this.languageIDNew = languageIDNew;
+	}
+
+	public Boolean getTemporaryBlockedFlag() {
+		return temporaryBlockedFlag;
+	}
+
+	public void setTemporaryBlockedFlag(Boolean temporaryBlockedFlag) {
+		this.temporaryBlockedFlag = temporaryBlockedFlag;
+	}
+
+	public Integer getUssdEndOfCallNotificationID() {
+		return ussdEndOfCallNotificationID;
+	}
+
+	public void setUssdEndOfCallNotificationID(
+			Integer ussdEndOfCallNotificationID) {
+		this.ussdEndOfCallNotificationID = ussdEndOfCallNotificationID;
+	}
+
+	public PamInformationList getPamInformationList() {
+		return pamInformationList;
+	}
+
+	public void setPamInformationList(PamInformationList pamInformationList) {
+		this.pamInformationList = pamInformationList;
 	}
 	
 }
