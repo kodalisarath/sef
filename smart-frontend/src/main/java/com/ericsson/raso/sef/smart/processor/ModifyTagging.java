285ad6243cec1ef6deeb49b61efcf71b23c0bfbc
ankittiwari0105 <ankit.tiwari@vekomy.com>
2014-06-12 02:04:56 +0700

    committed modify taggingfor unknown subscriber.

--- smart-frontend/src/main/java/com/ericsson/raso/sef/smart/processor/ModifyTagging.java
+++ smart-frontend/src/main/java/com/ericsson/raso/sef/smart/processor/ModifyTagging.java
@@ -130,6 +130,7 @@ public class ModifyTagging implements Processor {
 		// Subscriber subscriber = subscriberManagement.getSubscriberProfile(customerId, null);
 		if (subscriberInfo != null && subscriberInfo.getStatus() != null && subscriberInfo.getStatus().getCode() == 504) {
 			throw new SmException(new ResponseCode(504, "Invalid Account - subscriber not found"));
+		
 		}
 		logger.debug("If I am here, then it means the subscriber exists or may be other problem......" );
 	}
