41423cfb2be814a37c1fd07c7e061192b0e069d1
ejosayr <ejosayr@localhost>
2014-06-12 01:26:25 +0800

    Removed the try catch

--- smart-frontend/src/main/java/com/ericsson/raso/sef/smart/processor/ModifyCustomerPreActive.java
+++ smart-frontend/src/main/java/com/ericsson/raso/sef/smart/processor/ModifyCustomerPreActive.java
@@ -36,145 +36,172 @@ import com.nsn.ossbss.charge_once.wsdl.entity.tis.xsd._1.TransactionResult;
 
 public class ModifyCustomerPreActive implements Processor {
 
-	private static final Logger logger = LoggerFactory.getLogger(ModifyCustomerPreActive.class);
+	private static final Logger logger = LoggerFactory
+			.getLogger(ModifyCustomerPreActive.class);
+
 	@Override
 	public void process(Exchange exchange) throws Exception {
-		
-			ModifyCustomerPreActiveRequest request = (ModifyCustomerPreActiveRequest) exchange.getIn().getBody();
 
-			IConfig config = SefCoreServiceResolver.getConfigService();
-			
-				//SubscriberManagement subscriberManagement = SmartContext.getSubscriberManagement();
-				//List<String> keys = new ArrayList<String>();
-				//keys.add("PreActiveEndDate");
-				
-				//Subscriber subscriber = subscriberManagement.getSubscriberProfile(request.getCustomerId(), keys);
-				String requestId = RequestContextLocalStore.get().getRequestId();
-				SubscriberInfo subscriberinfo = readSubscriber(requestId, request.getCustomerId(),null);
-				
-				logger.info("subscriberinfo for msisdn:" + request.getCustomerId() + " is " + subscriberinfo);
+		ModifyCustomerPreActiveRequest request = (ModifyCustomerPreActiveRequest) exchange
+				.getIn().getBody();
 
-				if(subscriberinfo ==null)
-				{
-					logger.error("Manila Log: Subscriber Not Found. msisdn: "
-							+ request.getCustomerId());
+		IConfig config = SefCoreServiceResolver.getConfigService();
 
-					throw ExceptionUtil.toTisException(ErrorCode.invalidAccount);
-					
-					//throw new SmException(new ResponseCode(ErrorCode.invalidAccount.getCode(), ErrorCode.invalidAccount.getMessage() +" :msisdn "+request.getCustomerId()));
-					
-				}
-				else if (!ContractState.apiValue(ContractState.PREACTIVE.name()).equals(subscriberinfo.getLocalState())) {
+		// SubscriberManagement subscriberManagement =
+		// SmartContext.getSubscriberManagement();
+		// List<String> keys = new ArrayList<String>();
+		// keys.add("PreActiveEndDate");
 
-					logger.error("Manila Log: Subscriber should be in preactive state to extend the preActiveEndDate. msisdn: "
-							+ request.getCustomerId());
-					//throw new SmException(new ResponseCode(ErrorCode.notPreActive.getCode(), ErrorCode.notPreActive.getMessage() +" :msisdn "+request.getCustomerId()));
+		// Subscriber subscriber =
+		// subscriberManagement.getSubscriberProfile(request.getCustomerId(),
+		// keys);
+		String requestId = RequestContextLocalStore.get().getRequestId();
+		SubscriberInfo subscriberinfo = readSubscriber(requestId,
+				request.getCustomerId(), null);
 
-					throw ExceptionUtil.toTisException(ErrorCode.notPreActive);
-				}
+		logger.info("subscriberinfo for msisdn:" + request.getCustomerId()
+				+ " is " + subscriberinfo);
 
-				Date preActiveEndDate = null;
-				String preActiveEndDateStr = null;
-				try {
-					
-					
-					//preActiveEndDateStr = getMetaValue(subscriber.getMetas(), SmartConstants.PREACTIVE_ENDDATE);
-					preActiveEndDateStr =subscriberinfo.getMetas().get(SmartConstants.PREACTIVE_ENDDATE);
-					String milliSecMultiplier =config.getValue("GLOBAL", SmartConstants.MILLISEC_MULTIPLIER);  
-					
-					preActiveEndDate = DateUtil.convertStringToDate(preActiveEndDateStr, config.getValue("GLOBAL", "dateFormat"));
-					
-					preActiveEndDate = new Date(preActiveEndDate.getTime() + Long.valueOf(request.getDaysOfExtension()) * Long.valueOf(milliSecMultiplier));
+		if (subscriberinfo == null) {
+			logger.error("Manila Log: Subscriber Not Found. msisdn: "
+					+ request.getCustomerId());
 
-					
-					logger.info("new PreActiveEndDate for msisdn:" + request.getCustomerId() + " is " + preActiveEndDate.toString());
-				} catch (Exception e) {
-					logger.error("Manila Log: Not a valid preActiveEnddate for msisdn: " + request.getCustomerId());
-					
-					throw ExceptionUtil.toTisException(ErrorCode.notValidPreActiveEndDate);
-					
-//throw new SmException(new ResponseCode(ErrorCode.notValidPreActiveEndDate.getCode(), ErrorCode.notValidPreActiveEndDate.getMessage() +" :preActiveEndDate "+preActiveEndDate));
+			throw ExceptionUtil.toTisException(ErrorCode.invalidAccount);
 
-				}
-				List<Meta> metas = new ArrayList<Meta>();
-				//DateToStringTransformer transformer = new DateToStringTransformer(SmartContext.getProperty(SmartContext.DATE_FORMAT));
-				preActiveEndDateStr = DateUtil.convertDateToString(preActiveEndDate, config.getValue("GLOBAL", "dateFormat"));
-				Meta preActiveEndDateMeta = new Meta(SmartConstants.PREACTIVE_ENDDATE, preActiveEndDateStr);
-				metas.add(preActiveEndDateMeta);
-				//metas.add(new Meta("federation-profile", "shelfLifeExtension"));
+			// throw new SmException(new
+			// ResponseCode(ErrorCode.invalidAccount.getCode(),
+			// ErrorCode.invalidAccount.getMessage()
+			// +" :msisdn "+request.getCustomerId()));
 
-				updateSubscriber(requestId, request.getCustomerId(), metas);
-				
-				CommandResponseData responseData = createResponse(preActiveEndDate,request.isTransactional());
-				/* 	 @To Do.  To be completed after scheduler is ready.*/
-				//RescheduleRecycleCommand command = new RescheduleRecycleCommand(subscriber, preActiveEndDate);
-				//command.execute();
-				exchange.getOut().setBody(responseData);
+		} else if (!ContractState.apiValue(ContractState.PREACTIVE.name())
+				.equals(subscriberinfo.getLocalState())) {
+
+			logger.error("Manila Log: Subscriber should be in preactive state to extend the preActiveEndDate. msisdn: "
+					+ request.getCustomerId());
+			// throw new SmException(new
+			// ResponseCode(ErrorCode.notPreActive.getCode(),
+			// ErrorCode.notPreActive.getMessage()
+			// +" :msisdn "+request.getCustomerId()));
+
+			throw ExceptionUtil.toTisException(ErrorCode.notPreActive);
+		}
+
+		Date preActiveEndDate = null;
+		String preActiveEndDateStr = null;
+
+		// preActiveEndDateStr = getMetaValue(subscriber.getMetas(),
+		// SmartConstants.PREACTIVE_ENDDATE);
+		preActiveEndDateStr = subscriberinfo.getMetas().get(
+				SmartConstants.PREACTIVE_ENDDATE);
+		String milliSecMultiplier = config.getValue("GLOBAL",
+				SmartConstants.MILLISEC_MULTIPLIER);
+
+		preActiveEndDate = DateUtil.convertStringToDate(preActiveEndDateStr,
+				config.getValue("GLOBAL", "dateFormat"));
+
+		preActiveEndDate = new Date(preActiveEndDate.getTime()
+				+ Long.valueOf(request.getDaysOfExtension())
+				* Long.valueOf(milliSecMultiplier));
+
+		logger.info("new PreActiveEndDate for msisdn:"
+				+ request.getCustomerId() + " is "
+				+ preActiveEndDate.toString());
+		List<Meta> metas = new ArrayList<Meta>();
+		// DateToStringTransformer transformer = new
+		// DateToStringTransformer(SmartContext.getProperty(SmartContext.DATE_FORMAT));
+		preActiveEndDateStr = DateUtil.convertDateToString(preActiveEndDate,
+				config.getValue("GLOBAL", "dateFormat"));
+		Meta preActiveEndDateMeta = new Meta(SmartConstants.PREACTIVE_ENDDATE,
+				preActiveEndDateStr);
+		metas.add(preActiveEndDateMeta);
+		// metas.add(new Meta("federation-profile", "shelfLifeExtension"));
+
+		updateSubscriber(requestId, request.getCustomerId(), metas);
+
+		CommandResponseData responseData = createResponse(preActiveEndDate,
+				request.isTransactional());
+		/* @To Do. To be completed after scheduler is ready. */
+		// RescheduleRecycleCommand command = new
+		// RescheduleRecycleCommand(subscriber, preActiveEndDate);
+		// command.execute();
+		exchange.getOut().setBody(responseData);
 	}
-	
 
-	private CommandResponseData createResponse(Date preActiveEndDate,boolean isTransactional) {
+	private CommandResponseData createResponse(Date preActiveEndDate,
+			boolean isTransactional) {
 		CommandResponseData responseData = new CommandResponseData();
 		CommandResult result = new CommandResult();
 		responseData.setCommandResult(result);
-		
+
 		OperationResult operationResult = new OperationResult();
 
-		if(isTransactional) {
+		if (isTransactional) {
 			TransactionResult transactionResult = new TransactionResult();
 			result.setTransactionResult(transactionResult);
 			transactionResult.getOperationResult().add(operationResult);
 		} else {
 			result.setOperationResult(operationResult);
 		}
-		
+
 		Operation operation = new Operation();
 		operation.setModifier("CustomerPreActive");
 		operation.setName("Modify");
 		operationResult.getOperation().add(operation);
-		
+
 		ParameterList parameterList = new ParameterList();
 		operation.setParameterList(parameterList);
-		
+
 		DateTimeParameter dateTimeParameter = new DateTimeParameter();
-		dateTimeParameter.setName(SmartConstants.RESPONSE_PREACTIVE_ENDDATE);      
-		
-		dateTimeParameter.setValue(DateUtil.convertDateToUTCtime(preActiveEndDate));
-		operation.getParameterList().getParameterOrBooleanParameterOrByteParameter().add(dateTimeParameter);
+		dateTimeParameter.setName(SmartConstants.RESPONSE_PREACTIVE_ENDDATE);
+
+		dateTimeParameter.setValue(DateUtil
+				.convertDateToUTCtime(preActiveEndDate));
+		operation.getParameterList()
+				.getParameterOrBooleanParameterOrByteParameter()
+				.add(dateTimeParameter);
 		return responseData;
 	}
-	private SubscriberInfo updateSubscriber(String requestId, String customer_id,List<Meta> metas) {
+
+	private SubscriberInfo updateSubscriber(String requestId,
+			String customer_id, List<Meta> metas) {
 		logger.info("Invoking update subscriber on tx-engine subscriber interface");
-		ISubscriberRequest iSubscriberRequest = SmartServiceResolver.getSubscriberRequest();
+		ISubscriberRequest iSubscriberRequest = SmartServiceResolver
+				.getSubscriberRequest();
 		SubscriberInfo subInfo = new SubscriberInfo();
 		SubscriberResponseStore.put(requestId, subInfo);
-		iSubscriberRequest.updateSubscriber(requestId,customer_id, metas);
-		ISemaphore semaphore = SefCoreServiceResolver.getCloudAwareCluster().getSemaphore(requestId);
+		iSubscriberRequest.updateSubscriber(requestId, customer_id, metas);
+		ISemaphore semaphore = SefCoreServiceResolver.getCloudAwareCluster()
+				.getSemaphore(requestId);
 		try {
-		semaphore.init(0);
-		semaphore.acquire();
-		} catch(InterruptedException e) {
-			
+			semaphore.init(0);
+			semaphore.acquire();
+		} catch (InterruptedException e) {
+
 		}
 		logger.info("Check if response received for update subscriber");
-		SubscriberInfo subscriberInfo = (SubscriberInfo) SubscriberResponseStore.remove(requestId);
+		SubscriberInfo subscriberInfo = (SubscriberInfo) SubscriberResponseStore
+				.remove(requestId);
 		return subscriberInfo;
 	}
-	
-	private SubscriberInfo readSubscriber(String requestId, String subscriberId, List<Meta> metas){
-		ISubscriberRequest iSubscriberRequest = SmartServiceResolver.getSubscriberRequest();
+
+	private SubscriberInfo readSubscriber(String requestId,
+			String subscriberId, List<Meta> metas) {
+		ISubscriberRequest iSubscriberRequest = SmartServiceResolver
+				.getSubscriberRequest();
 		SubscriberInfo subInfo = new SubscriberInfo();
 		SubscriberResponseStore.put(requestId, subInfo);
 		iSubscriberRequest.readSubscriber(requestId, subscriberId, metas);
-		ISemaphore semaphore = SefCoreServiceResolver.getCloudAwareCluster().getSemaphore(requestId);
+		ISemaphore semaphore = SefCoreServiceResolver.getCloudAwareCluster()
+				.getSemaphore(requestId);
 		try {
-		semaphore.init(0);
-		semaphore.acquire();
-		} catch(InterruptedException e) {
+			semaphore.init(0);
+			semaphore.acquire();
+		} catch (InterruptedException e) {
 		}
 		logger.info("Check if response received for read subscriber");
-		SubscriberInfo subscriberInfo = (SubscriberInfo) SubscriberResponseStore.remove(requestId);
+		SubscriberInfo subscriberInfo = (SubscriberInfo) SubscriberResponseStore
+				.remove(requestId);
 		return subscriberInfo;
-		
+
 	}
 }
