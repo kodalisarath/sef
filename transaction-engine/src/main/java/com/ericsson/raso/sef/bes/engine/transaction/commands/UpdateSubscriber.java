9198d703e4d9ebb399386ada1515c58ee9a803a1
vinay548 <vinayreddy548@gmail.com>
2014-06-12 05:04:00 +0530

    toString of an object returned.

--- transaction-engine/src/main/java/com/ericsson/raso/sef/bes/engine/transaction/commands/UpdateSubscriber.java
+++ transaction-engine/src/main/java/com/ericsson/raso/sef/bes/engine/transaction/commands/UpdateSubscriber.java
@@ -21,15 +21,18 @@ import com.ericsson.raso.sef.core.db.service.SubscriberService;
 import com.ericsson.sef.bes.api.entities.TransactionStatus;
 import com.ericsson.sef.bes.api.subscriber.ISubscriberResponse;
 
-public class UpdateSubscriber extends AbstractTransaction{
-	static final String PRE_ACTIVE="PRE_ACTIVE";
-	
+public class UpdateSubscriber extends AbstractTransaction {
+	static final String PRE_ACTIVE = "PRE_ACTIVE";
+
 	private static final long serialVersionUID = 7686721923498952231L;
-	private static final Logger LOGGER = LoggerFactory.getLogger(UpdateSubscriber.class);
-	
-	public UpdateSubscriber(String requestId,String subscriberId,Map<String,String> metas) {
-		super(requestId, new UpdateSubscriberRequest(requestId, subscriberId,metas));
-		this.setResponse(new UpdateSubscriberResponse(requestId,true));
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(UpdateSubscriber.class);
+
+	public UpdateSubscriber(String requestId, String subscriberId,
+			Map<String, String> metas) {
+		super(requestId, new UpdateSubscriberRequest(requestId, subscriberId,
+				metas));
+		this.setResponse(new UpdateSubscriberResponse(requestId, true));
 		LOGGER.debug("FFE Sanity Check: " + subscriberId + ", " + metas);
 	}
 
@@ -37,146 +40,181 @@ public class UpdateSubscriber extends AbstractTransaction{
 	public Boolean execute() throws TransactionException {
 		LOGGER.debug("Entering Update Subscriber...");
 		com.ericsson.raso.sef.core.db.model.Subscriber subscriberEntity = null;
-		
+
 		try {
-			//This enity must contains the subscriber and his  metas from the DB
-			subscriberEntity = ((UpdateSubscriberRequest)this.getRequest()).persistableEntity();
-			LOGGER.debug("PIN"+subscriberEntity.getPin()+"   "+subscriberEntity.getContractState()+"  "+subscriberEntity.getAccountId()+" "+subscriberEntity.getMsisdn());
+			// This enity must contains the subscriber and his metas from the DB
+			subscriberEntity = ((UpdateSubscriberRequest) this.getRequest())
+					.persistableEntity();
+			LOGGER.debug("PIN" + subscriberEntity.getPin() + "   "
+					+ subscriberEntity.getContractState() + "  "
+					+ subscriberEntity.getAccountId() + " "
+					+ subscriberEntity.getMsisdn());
 			LOGGER.debug(subscriberEntity.toString());
-			SubscriberService subscriberStore = SefCoreServiceResolver.getSusbcriberStore();
+			SubscriberService subscriberStore = SefCoreServiceResolver
+					.getSusbcriberStore();
 			if (subscriberStore == null) {
 				LOGGER.error("Unable to access persistence tier service!!");
-				this.getResponse().setReturnFault(new TransactionException(this.getRequestId(), 
-										new ResponseCode(1006, "Unable to access persistence tier service!! Check configuration (beans.xml)")));
+				this.getResponse()
+						.setReturnFault(
+								new TransactionException(
+										this.getRequestId(),
+										new ResponseCode(1006,
+												"Unable to access persistence tier service!! Check configuration (beans.xml)")));
 				return false;
 			}
-			if(subscriberEntity == null) {
-				LOGGER.error("Subscriber not found in database" );
-				
-				this.getResponse().setReturnFault(new TransactionException("tx-engine", new ResponseCode(504,"Subscriber not found")));
+			if (subscriberEntity == null) {
+				LOGGER.error("Subscriber not found in database");
+
+				this.getResponse().setReturnFault(
+						new TransactionException("tx-engine", new ResponseCode(
+								504, "Subscriber not found")));
 				sendResponse();
 				return false;
-			}
-			else{
-				LOGGER.debug("Enum "+ContractState.apiValue("PRE_ACTIVE"));
-				LOGGER.debug("Meta contract state from "+subscriberEntity.getContractState().toString());
-			    LOGGER.debug("Checking size of meta  from DB"+subscriberEntity.getMetas().size());
-			    List<Meta> listMetas = ((UpdateSubscriberRequest)this.getRequest()).getRequestMetas();
-			    LOGGER.debug("Check for the metas from processors"+listMetas.size());
-				if(ContractState.apiValue("PRE_ACTIVE").equals(subscriberEntity.getContractState().toString())){
-					LOGGER.debug("Meta list from the processor"+listMetas.size());
+			} else {
+				LOGGER.debug("Enum " + ContractState.apiValue("PRE_ACTIVE"));
+				LOGGER.debug("Meta contract state from "
+						+ subscriberEntity.getContractState().toString());
+				LOGGER.debug("Checking size of meta  from DB"
+						+ subscriberEntity.getMetas().size());
+				List<Meta> listMetas = ((UpdateSubscriberRequest) this
+						.getRequest()).getRequestMetas();
+				LOGGER.debug("Check for the metas from processors"
+						+ listMetas.size());
+				if (ContractState.apiValue("PRE_ACTIVE").toString().equals(
+						subscriberEntity.getContractState().toString())) {
+					LOGGER.debug("Meta list from the processor"
+							+ listMetas.size());
 					LOGGER.debug("Iterating the metas from then processor");
-					for(Meta meta:listMetas){
-						if(subscriberEntity.getMetas().contains(meta)){
+					for (Meta meta : listMetas) {
+						if (subscriberEntity.getMetas().contains(meta)) {
 							try {
-								subscriberStore.updateMeta(this.getRequestId(),subscriberEntity.getMsisdn(),meta);
+								subscriberStore.updateMeta(this.getRequestId(),
+										subscriberEntity.getMsisdn(), meta);
 							} catch (PersistenceError e) {
-								LOGGER.error("Error in the updatemeta at UpdateSubscriber",e);
+								LOGGER.error(
+										"Error in the updatemeta at UpdateSubscriber",
+										e);
 							}
-						}else{
+						} else {
 							try {
 								LOGGER.debug("Metas doesnot contain in the DB,creating now!!!!");
-								subscriberStore.createMeta(this.getRequestId(), subscriberEntity.getMsisdn(),meta);
+								subscriberStore.createMeta(this.getRequestId(),
+										subscriberEntity.getMsisdn(), meta);
 							} catch (PersistenceError e) {
-								LOGGER.error("Error in the createmeta at UpdateSubscriber",e);
+								LOGGER.error(
+										"Error in the createmeta at UpdateSubscriber",
+										e);
 							}
 						}
-						
+
 					}
-					
-				}else{
-					
-					this.getResponse().setReturnFault(new TransactionException("tx-engine", new ResponseCode(4020,"Invalid Operation State")));
-					
+
+				} else {
+
+					this.getResponse().setReturnFault(
+							new TransactionException("tx-engine",
+									new ResponseCode(4020,
+											"Invalid Operation State")));
+
 				}
 			}
-			LOGGER.debug("Got Persistable Entity: Subscriber: " + subscriberEntity);
-				
+			LOGGER.debug("Got Persistable Entity: Subscriber: "
+					+ subscriberEntity);
+
 		} catch (FrameworkException e1) {
-			LOGGER.error("Method:Update Subscriber framework Exception",e1);
-			this.getResponse().setReturnFault(new TransactionException(this.getRequestId(), 
-											new ResponseCode(1006, "Unable to pack the workflow tasks for this use-case"), e1));
+			LOGGER.error("Method:Update Subscriber framework Exception", e1);
+			this.getResponse()
+					.setReturnFault(
+							new TransactionException(
+									this.getRequestId(),
+									new ResponseCode(1006,
+											"Unable to pack the workflow tasks for this use-case"),
+									e1));
 			sendResponse();
 			return false;
 		}
-		
-		
-		/*if (subscriberStore == null) {
-			LOGGER.error("Unable to access persistence tier service!!");
-			this.getResponse().setReturnFault(new TransactionException(this.getRequestId(), 
-									new ResponseCode(1006, "Unable to access persistence tier service!! Check configuration (beans.xml)")));
-			return false;
-		}*/
-		//It contains the metas we get from the processor
-	
-		
-	/*	LOGGER.debug("About to persist Subscriber: " + subscriberEntity);
-		try {
-			subscriberStore.updateSubscriber(this.getRequestId(), subscriberEntity);
-		} catch (PersistenceError e1) {
-			String errorCode;
-			LOGGER.debug("Persistence error while updating subscriber",e1);
-			if(e1.getStatusCode().getCode() == 9000){
-				errorCode="504";
-			}else{
-				errorCode="4020";
-			}
-			this.getResponse().setReturnFault(new TransactionException(this.getRequestId(), new ResponseCode(Integer.valueOf(errorCode), "Saving metas failed!!"), e1));
-			return false;
-		}
-		LOGGER.debug("Update Subscriber successfull!!");
-		LOGGER.debug("calling setMetas ");
-		try {
-			subscriberStore.updateMetas(this.getRequestId(), subscriberEntity.getUserId(),
-						TransactionServiceHelper.getSefCoreList(((UpdateSubscriberRequest)this.getRequest()).getMetas()));
-			
-		} catch (PersistenceError e) {
-			LOGGER.debug("Persistence error while updating subscriber metas",e);
-			this.getResponse().setReturnFault(new TransactionException(this.getRequestId(), new ResponseCode(1004, "Saving metas failed!!"), e));
-			return false;
-		}*/
+
+		/*
+		 * if (subscriberStore == null) {
+		 * LOGGER.error("Unable to access persistence tier service!!");
+		 * this.getResponse().setReturnFault(new
+		 * TransactionException(this.getRequestId(), new ResponseCode(1006,
+		 * "Unable to access persistence tier service!! Check configuration (beans.xml)"
+		 * ))); return false; }
+		 */
+		// It contains the metas we get from the processor
+
+		/*
+		 * LOGGER.debug("About to persist Subscriber: " + subscriberEntity); try
+		 * { subscriberStore.updateSubscriber(this.getRequestId(),
+		 * subscriberEntity); } catch (PersistenceError e1) { String errorCode;
+		 * LOGGER.debug("Persistence error while updating subscriber",e1);
+		 * if(e1.getStatusCode().getCode() == 9000){ errorCode="504"; }else{
+		 * errorCode="4020"; } this.getResponse().setReturnFault(new
+		 * TransactionException(this.getRequestId(), new
+		 * ResponseCode(Integer.valueOf(errorCode), "Saving metas failed!!"),
+		 * e1)); return false; }
+		 * LOGGER.debug("Update Subscriber successfull!!");
+		 * LOGGER.debug("calling setMetas "); try {
+		 * subscriberStore.updateMetas(this.getRequestId(),
+		 * subscriberEntity.getUserId(),
+		 * TransactionServiceHelper.getSefCoreList(
+		 * ((UpdateSubscriberRequest)this.getRequest()).getMetas()));
+		 * 
+		 * } catch (PersistenceError e) {
+		 * LOGGER.debug("Persistence error while updating subscriber metas",e);
+		 * this.getResponse().setReturnFault(new
+		 * TransactionException(this.getRequestId(), new ResponseCode(1004,
+		 * "Saving metas failed!!"), e)); return false; }
+		 */
 		sendResponse();
-		
+
 		return true;
 	}
 
 	@Override
 	public void sendResponse() {
-		
+
 		LOGGER.debug("Invoking update subscriber response ");
-		TransactionStatus txnStatus=new TransactionStatus();
+		TransactionStatus txnStatus = new TransactionStatus();
 		boolean result = true;
 		if (this.getResponse() != null) {
 			if (this.getResponse().getAtomicStepResults() != null) {
-				for (AbstractStepResult stepResult: this.getResponse().getAtomicStepResults().values()) {
+				for (AbstractStepResult stepResult : this.getResponse()
+						.getAtomicStepResults().values()) {
 					if (stepResult.getResultantFault() != null) {
-						txnStatus.setComponent(stepResult.getResultantFault().getComponent());
-						txnStatus.setCode(stepResult.getResultantFault().getStatusCode().getCode());
-						txnStatus.setDescription(stepResult.getResultantFault().getStatusCode().getMessage());
-						LOGGER.debug("UpdateSubscriber::=> Transaction Status: " + txnStatus);
+						txnStatus.setComponent(stepResult.getResultantFault()
+								.getComponent());
+						txnStatus.setCode(stepResult.getResultantFault()
+								.getStatusCode().getCode());
+						txnStatus.setDescription(stepResult.getResultantFault()
+								.getStatusCode().getMessage());
+						LOGGER.debug("UpdateSubscriber::=> Transaction Status: "
+								+ txnStatus);
 						result = false;
 						break;
 					}
 				}
 			}
-		}else
-			result=false;
-		
+		} else
+			result = false;
+
 		if (result != false)
 			result = true;
 		LOGGER.debug("UpdateSubscriber::=> Functional Result: " + result);
-		
+
 		LOGGER.debug("Invoking update subscriber response!!");
-		ISubscriberResponse subscriberClient = ServiceResolver.getSubscriberResponseClient();
+		ISubscriberResponse subscriberClient = ServiceResolver
+				.getSubscriberResponseClient();
 		if (subscriberClient != null) {
 			LOGGER.debug("Gonna Send send response");
-			subscriberClient.updateSubscriber(this.getRequestId(), txnStatus, result);
+			subscriberClient.updateSubscriber(this.getRequestId(), txnStatus,
+					result);
 			LOGGER.debug("update susbcriber response posted");
 		} else {
 			LOGGER.error("Unable to acquire client access to response interface. Request will time-out in the consumer side!!");
 		}
 		LOGGER.debug("update susbcriber response posted");
 	}
-
 
 }
