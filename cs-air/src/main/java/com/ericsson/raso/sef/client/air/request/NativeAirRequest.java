b887f3506d5b90329f4fe7f351cc6f539658f979
Sathya Narayanan S <sathya.narayanan.s@ericsson.com>
2014-06-13 03:47:53 +0800

    Serialization impedence fixed...

--- cs-air/src/main/java/com/ericsson/raso/sef/client/air/request/NativeAirRequest.java
+++ cs-air/src/main/java/com/ericsson/raso/sef/client/air/request/NativeAirRequest.java
@@ -1,10 +1,12 @@
 package com.ericsson.raso.sef.client.air.request;
 
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
-public abstract class NativeAirRequest {
-	
+public abstract class NativeAirRequest implements Serializable {
+	private static final long serialVersionUID = -6218804399266839105L;
+
 	private Map<String, Object> paramMap = new HashMap<String, Object>();
 	
 	public Map<String, Object> toNative() {
--- cs-air/src/main/java/com/ericsson/raso/sef/client/air/request/OfferSelection.java
+++ cs-air/src/main/java/com/ericsson/raso/sef/client/air/request/OfferSelection.java
@@ -1,6 +1,7 @@
 package com.ericsson.raso.sef.client.air.request;
 
 public class OfferSelection extends NativeAirRequest {
+	private static final long serialVersionUID = 8388331634098443476L;
 
 	private int offerIDFirst;
 	private int offerIDLast;
