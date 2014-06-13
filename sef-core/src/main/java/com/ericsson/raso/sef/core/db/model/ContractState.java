c776a7918c4858bc8991aa54c6a046d30769157d
Sathya Narayanan S <sathya.narayanan.s@ericsson.com>
2014-06-14 01:48:21 +0800

    Updated life-cycle model

--- sef-core/src/main/java/com/ericsson/raso/sef/core/db/model/ContractState.java
+++ sef-core/src/main/java/com/ericsson/raso/sef/core/db/model/ContractState.java
@@ -1,32 +1,41 @@
 package com.ericsson.raso.sef.core.db.model;
 
 public enum ContractState {
-	PREACTIVE("PRE_ACTIVE"), ACTIVE("ACTIVE"), RECYCLED("DEACTIVE"), BARRED("SUSPENDED"), GRACE("GRACE"), READY_TO_DELETE("READY_TO_DELETE"), NONE("None");
-	
+	PREACTIVE("PRE_ACTIVE"),
+	ACTIVE("ACTIVE"),
+	RECYCLED("DEACTIVE"),
+	BARRED("SUSPENDED"),
+	GRACE("GRACE"),
+	READY_TO_DELETE("READY_TO_DELETE"),
+	DUNNING ("DELETED"),
+	NONE("None");
+
 	private String name;
 
 	private ContractState(String name) {
 		this.name = name;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public static ContractState apiValue(String name) {
 
-		if(name.equalsIgnoreCase("PRE_ACTIVE")) {
+		if (name.equalsIgnoreCase("PRE_ACTIVE")) {
 			return PREACTIVE;
 		} else if (name.equalsIgnoreCase("ACTIVE")) {
 			return ACTIVE;
-		} else if(name.equalsIgnoreCase("DEACTIVE")) {
+		} else if (name.equalsIgnoreCase("DEACTIVE")) {
 			return RECYCLED;
-		} else if(name.equalsIgnoreCase("SUSPENDED")) {
+		} else if (name.equalsIgnoreCase("SUSPENDED")) {
 			return BARRED;
-		} else if(name.equalsIgnoreCase("GRACE")) {
+		} else if (name.equalsIgnoreCase("GRACE")) {
 			return GRACE;
-		} else if(name.equalsIgnoreCase("READY_TO_DELETE")) {
+		} else if (name.equalsIgnoreCase("READY_TO_DELETE")) {
 			return READY_TO_DELETE;
+		} else if (name.equalsIgnoreCase("DELETED")) {
+			return DUNNING;
 		} else {
 			return NONE;
 		}
