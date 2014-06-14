b2c62211255fca5dcdcce8935e3034073f4a584f
vinay548 <vinayreddy548@gmail.com>
2014-06-14 19:27:45 +0530

    Fixed equals error in contract state comparision

--- transaction-engine/src/main/java/com/ericsson/raso/sef/bes/engine/transaction/commands/UpdateSubscriber.java
+++ transaction-engine/src/main/java/com/ericsson/raso/sef/bes/engine/transaction/commands/UpdateSubscriber.java
@@ -67,8 +67,11 @@ case Constants.VersionCreateOrWriteRop:
 					this.getResponse().setReturnFault(new TransactionException("tx-engine", new ResponseCode(504, "Subscriber not found")));
 					sendResponse();
 				} else {
+					LOGGER.debug("Printing Subscriber entity "+subscriberEntity.toString());
 					LOGGER.debug("Subscriber exists and checking for the preactive state");
-					if (ContractState.apiValue("PRE_ACTIVE").toString().equals(subscriberEntity.getContractState().toString())) {
+					LOGGER.debug("checking if this equals working fine ");
+					if (ContractState.apiValue("PRE_ACTIVE") == subscriberEntity.getContractState()) {
+						LOGGER.debug("It is PRE_ACTIVE state");
 						for (Meta meta : listMetas) {
 							if (subscriberEntity.getMetas().contains(meta)) {
 								try {
@@ -90,11 +93,9 @@ case Constants.VersionCreateOrWriteRop:
 						}
 
 					} else {
-
 						this.getResponse().setReturnFault(
 								new TransactionException("tx-engine",new ResponseCode(4020,"Invalid Operation State")));
 						sendResponse();
-
 					}
 				}
 				
