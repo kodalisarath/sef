46c72276506cee3414c4c8f6568d5a8eccfea838
n0th1n6 <nrafallo@gmail.com>
2014-06-15 03:37:14 +0800

    ModifyTagging

--- smart-frontend/src/main/java/com/ericsson/raso/sef/smart/processor/ModifyTagging.java
+++ smart-frontend/src/main/java/com/ericsson/raso/sef/smart/processor/ModifyTagging.java
@@ -14,6 +14,7 @@ import com.ericsson.raso.sef.core.RequestContextLocalStore;
 import com.ericsson.raso.sef.core.ResponseCode;
 import com.ericsson.raso.sef.core.SefCoreServiceResolver;
 import com.ericsson.raso.sef.core.SmException;
+import com.ericsson.raso.sef.smart.ErrorCode;
 import com.ericsson.raso.sef.smart.ExceptionUtil;
 import com.ericsson.raso.sef.smart.SmartServiceResolver;
 import com.ericsson.raso.sef.smart.subscriber.response.SubscriberInfo;
@@ -30,166 +31,167 @@ import com.nsn.ossbss.charge_once.wsdl.entity.tis.xsd._1.OperationResult;
 import com.nsn.ossbss.charge_once.wsdl.entity.tis.xsd._1.ParameterList;
 import com.nsn.ossbss.charge_once.wsdl.entity.tis.xsd._1.TransactionResult;
 
-public class ModifyTagging implements Processor {private static final Logger logger = LoggerFactory
-.getLogger(ModifyTagging.class);
+public class ModifyTagging implements Processor {
+	private static final Logger logger = LoggerFactory.getLogger(ModifyTagging.class);
 
-@SuppressWarnings("unchecked")
-@Override
-public void process(Exchange exchange) throws SmException {
-ModifyTaggingRequest request = (ModifyTaggingRequest) exchange
-		.getIn().getBody();
-String requestId = RequestContextLocalStore.get().getRequestId();
-Integer tag = Integer.valueOf(request.getTagging());
-logger.error("Subscriber Tagging Code is:", tag);
+	@SuppressWarnings("unchecked")
+	@Override
+	public void process(Exchange exchange) throws SmException {
+		ModifyTaggingRequest request = (ModifyTaggingRequest) exchange
+				.getIn().getBody();
+		String requestId = RequestContextLocalStore.get().getRequestId();
+		Integer tag = Integer.valueOf(request.getTagging());
+		logger.error("Subscriber Tagging Code is:", tag);
 
-List<Meta> metas = new ArrayList<Meta>();
-metas.add(new Meta("CustomerId", request.getCustomerId()));
-metas.add(new Meta("AccessKey", request.getAccessKey()));
-metas.add(new Meta("Tagging", String.valueOf(request.getTagging())));
-metas.add(new Meta("EventInfo", String.valueOf(request.getEventInfo())));
-metas.add(new Meta("MessageId", String.valueOf(request.getMessageId())));
+		List<Meta> metas = new ArrayList<Meta>();
+		metas.add(new Meta("CustomerId", request.getCustomerId()));
+		metas.add(new Meta("AccessKey", request.getAccessKey()));
+		metas.add(new Meta("Tagging", String.valueOf(request.getTagging())));
+		metas.add(new Meta("EventInfo", String.valueOf(request.getEventInfo())));
+		metas.add(new Meta("MessageId", String.valueOf(request.getMessageId())));
 
-String tagging = String.valueOf(request.getTagging());
-switch (tagging) {
-case "0":
-	metas.add(new Meta("HANDLE_LIFE_CYCLE", "resetBit"));
-	break;
-case "1":
-	metas.add(new Meta("HANDLE_LIFE_CYCLE", "forcedDeleteBit"));
-	break;
-case "2":
-	metas.add(new Meta("HANDLE_LIFE_CYCLE", "barGeneralBit"));
-	break;
-case "3":
-	metas.add(new Meta("HANDLE_LIFE_CYCLE", "barIrmBit"));
-	break;
-case "4":
-	metas.add(new Meta("HANDLE_LIFE_CYCLE", "barOtherBit"));
-	break;
-case "5":
-	metas.add(new Meta("HANDLE_LIFE_CYCLE", "specialFraudBit"));
-	break;
-case "6":
-	metas.add(new Meta("HANDLE_LIFE_CYCLE", "accountBlockingBit"));
-	break;
-case "7":
-	metas.add(new Meta("HANDLE_LIFE_CYCLE", "recycleBit"));
-	break;
-default:
-	metas.add(new Meta("HANDLE_LIFE_CYCLE", "invalidBit"));
-	break;
-}
-logger.debug("Usecase Metas: " + metas);
+		String tagging = String.valueOf(request.getTagging());
+		switch (tagging) {
+		case "0":
+			metas.add(new Meta("HANDLE_LIFE_CYCLE", "resetBit"));
+			break;
+		case "1":
+			metas.add(new Meta("HANDLE_LIFE_CYCLE", "forcedDeleteBit"));
+			break;
+		case "2":
+			metas.add(new Meta("HANDLE_LIFE_CYCLE", "barGeneralBit"));
+			break;
+		case "3":
+			metas.add(new Meta("HANDLE_LIFE_CYCLE", "barIrmBit"));
+			break;
+		case "4":
+			metas.add(new Meta("HANDLE_LIFE_CYCLE", "barOtherBit"));
+			break;
+		case "5":
+			metas.add(new Meta("HANDLE_LIFE_CYCLE", "specialFraudBit"));
+			break;
+		case "6":
+			metas.add(new Meta("HANDLE_LIFE_CYCLE", "accountBlockingBit"));
+			break;
+		case "7":
+			metas.add(new Meta("HANDLE_LIFE_CYCLE", "recycleBit"));
+			break;
+		default:
+			metas.add(new Meta("HANDLE_LIFE_CYCLE", "invalidBit"));
+			break;
+		}
+		logger.debug("Usecase Metas: " + metas);
 
-SubscriberInfo updateSubscriberInfo;
+		SubscriberInfo updateSubscriberInfo;
 
-updateSubscriberInfo = updateSubscriber(requestId,
-		request.getCustomerId(), metas,Constants.ModiFyTagging);
+		ISubscriberRequest iSubscriberRequest = SmartServiceResolver.getSubscriberRequest();
 
-if (updateSubscriberInfo.getStatus().getCode() == 0) {
-	Collection<Meta> updateInfoMetasMap = (Collection<Meta>) updateSubscriberInfo.getMetas();
-	List<Meta> updateInfoMetaList = convertToMetaList(updateInfoMetasMap);
-	updateSubscriberInfo = updateSubscriberHandleLifeCycle(updateSubscriberInfo.getRequestId(),updateSubscriberInfo.getMsisdn(), updateInfoMetaList);
-	if (updateSubscriberInfo.getStatus() != null) {
-		throw ExceptionUtil.toSmException(new ResponseCode(updateSubscriberInfo.getStatus().getCode(),updateSubscriberInfo.getStatus().getDescription()));
-	}else{
-		DummyProcessor.response(exchange);
-	} 
-}else if(updateSubscriberInfo.getStatus() != null){
-	throw ExceptionUtil.toSmException(new ResponseCode(updateSubscriberInfo.getStatus().getCode(),updateSubscriberInfo.getStatus().getDescription()));
-}
-exchange.getOut().setBody(createResponse(tag, request.isTransactional()));
-}
+		updateSubscriberInfo = updateSubscriber(requestId, request.getCustomerId(), metas,Constants.ModiFyTagging);
 
-private CommandResponseData createResponse(int tag, boolean isTransactional) {
-CommandResponseData responseData = new CommandResponseData();
-CommandResult result = new CommandResult();
-responseData.setCommandResult(result);
+		if (updateSubscriberInfo.getStatus().getCode() == 0) {
+			Collection<Meta> updateInfoMetasMap = (Collection<Meta>) updateSubscriberInfo.getMetas();
+			List<Meta> updateInfoMetaList = convertToMetaList(updateInfoMetasMap);
+			updateSubscriberInfo = updateSubscriberHandleLifeCycle(updateSubscriberInfo.getRequestId(),updateSubscriberInfo.getMsisdn(), updateInfoMetaList);
+			if (updateSubscriberInfo.getStatus() != null) {
+				throw ExceptionUtil.toSmException(new ResponseCode(updateSubscriberInfo.getStatus().getCode(),updateSubscriberInfo.getStatus().getDescription()));
+			}else{
+				DummyProcessor.response(exchange);
+			} 
+		} else if(updateSubscriberInfo.getStatus() != null){
+			throw ExceptionUtil.toSmException(new ResponseCode(updateSubscriberInfo.getStatus().getCode(),updateSubscriberInfo.getStatus().getDescription()));
+		}
+		exchange.getOut().setBody(createResponse(tag, request.isTransactional()));
+	}
 
-OperationResult operationResult = new OperationResult();
+	private CommandResponseData createResponse(int tag, boolean isTransactional) {
+		CommandResponseData responseData = new CommandResponseData();
+		CommandResult result = new CommandResult();
+		responseData.setCommandResult(result);
 
-if (isTransactional) {
-TransactionResult transactionResult = new TransactionResult();
-result.setTransactionResult(transactionResult);
-transactionResult.getOperationResult().add(operationResult);
-} else {
-result.setOperationResult(operationResult);
-}
+		OperationResult operationResult = new OperationResult();
 
-Operation operation = new Operation();
-operation.setModifier("Tagging");
-operation.setName("Modify");
-operationResult.getOperation().add(operation);
+		if (isTransactional) {
+			TransactionResult transactionResult = new TransactionResult();
+			result.setTransactionResult(transactionResult);
+			transactionResult.getOperationResult().add(operationResult);
+		} else {
+			result.setOperationResult(operationResult);
+		}
 
-ParameterList parameterList = new ParameterList();
-List<Object> dataSetList = parameterList.getParameterOrBooleanParameterOrByteParameter();
+		Operation operation = new Operation();
+		operation.setModifier("Tagging");
+		operation.setName("Modify");
+		operationResult.getOperation().add(operation);
 
-IntParameter intParameter = new IntParameter();
-intParameter.setName("ResultTagging");
-intParameter.setValue(tag);
-dataSetList.add(intParameter);
+		ParameterList parameterList = new ParameterList();
+		List<Object> dataSetList = parameterList.getParameterOrBooleanParameterOrByteParameter();
 
-operation.setParameterList(parameterList);
+		IntParameter intParameter = new IntParameter();
+		intParameter.setName("ResultTagging");
+		intParameter.setValue(tag);
+		dataSetList.add(intParameter);
 
-return responseData;
-}
+		operation.setParameterList(parameterList);
+
+		return responseData;
+	}
 
 
-private SubscriberInfo updateSubscriber(String requestId,
-String customer_id, List<Meta> metas,String processRequestKey) throws SmException {
-logger.info("Invoking update subscriber on tx-engine subscriber interface");
-ISubscriberRequest iSubscriberRequest = SmartServiceResolver
-	.getSubscriberRequest();
-SubscriberInfo subInfo = new SubscriberInfo();
-SubscriberResponseStore.put(requestId, subInfo);
-logger.debug("Requesting ");
-iSubscriberRequest.updateSubscriber(requestId, customer_id, metas,processRequestKey);
-ISemaphore semaphore = SefCoreServiceResolver.getCloudAwareCluster()
-	.getSemaphore(requestId);
-try {
-semaphore.init(0);
-semaphore.acquire();
-} catch (InterruptedException e) {
-logger.error("Error while calling acquire()");
-}
-logger.info("Check if response received for update subscriber");
-SubscriberInfo subscriberInfo = (SubscriberInfo) SubscriberResponseStore
-	.remove(requestId);
-return subscriberInfo;
-}
+	private SubscriberInfo updateSubscriber(String requestId, String customer_id, List<Meta> metas,String processRequestKey) throws SmException {
+		logger.info("Invoking update subscriber on tx-engine subscriber interface");
+		ISubscriberRequest iSubscriberRequest = SmartServiceResolver
+				.getSubscriberRequest();
+		SubscriberInfo subInfo = new SubscriberInfo();
+		SubscriberResponseStore.put(requestId, subInfo);
+		logger.debug("Requesting ");
+		iSubscriberRequest.updateSubscriber(requestId, customer_id, metas,processRequestKey);
+		ISemaphore semaphore = SefCoreServiceResolver.getCloudAwareCluster()
+				.getSemaphore(requestId);
+		try {
+			semaphore.init(0);
+			semaphore.acquire();
+		} catch (InterruptedException e) {
+			logger.error("Error while calling acquire()");
+		}
+		logger.info("Check if response received for update subscriber");
+		SubscriberInfo subscriberInfo = (SubscriberInfo) SubscriberResponseStore
+				.remove(requestId);
+		return subscriberInfo;
+	}
 
-private SubscriberInfo updateSubscriberHandleLifeCycle(String requestId, String msisdn, List<Meta> metas) {
-	logger.info("Invoking update subscriber on tx-engine subscriber interface for hande modifyhandle life cycle");
-	
-	ISubscriberRequest iSubscriberRequest = SmartServiceResolver.getSubscriberRequest();
-	SubscriberInfo subInfo = new SubscriberInfo();
-	
-	SubscriberResponseStore.put(requestId, subInfo);
-	iSubscriberRequest.handleLifeCycle(requestId, msisdn, null, metas);
-	
-	ISemaphore semaphore = SefCoreServiceResolver.getCloudAwareCluster().getSemaphore(requestId);
-	try {
-		semaphore.init(0);
-		semaphore.acquire();
-	} catch (InterruptedException e) {
+	private SubscriberInfo updateSubscriberHandleLifeCycle(String requestId, String msisdn, List<Meta> metas) {
+		logger.info("Invoking update subscriber on tx-engine subscriber interface for hande modifyhandle life cycle");
+
+		ISubscriberRequest iSubscriberRequest = SmartServiceResolver.getSubscriberRequest();
+		SubscriberInfo subInfo = new SubscriberInfo();
+
+		SubscriberResponseStore.put(requestId, subInfo);
+		iSubscriberRequest.handleLifeCycle(requestId, msisdn, null, metas);
+
+		ISemaphore semaphore = SefCoreServiceResolver.getCloudAwareCluster().getSemaphore(requestId);
+		try {
+			semaphore.init(0);
+			semaphore.acquire();
+		} catch (InterruptedException e) {
+
+		}
+		logger.info("Check if response received for update subscriber");
+		SubscriberInfo subscriberInfo = (SubscriberInfo) SubscriberResponseStore.remove(requestId);
+		return subscriberInfo;
 
 	}
-	logger.info("Check if response received for update subscriber");
-	SubscriberInfo subscriberInfo = (SubscriberInfo) SubscriberResponseStore.remove(requestId);
-	return subscriberInfo;
+	private List<Meta> convertToMetaList(Collection<Meta> subscriberMetas) {
+		List<Meta> metaList=new ArrayList<Meta>();
+		if(subscriberMetas != null){
+			List<Meta> subscribermetaList=new ArrayList<Meta>(subscriberMetas);
+			for(Meta metaSubscriber:subscribermetaList){
+				Meta meta=new Meta();
+				meta.setKey(metaSubscriber.getKey());
+				meta.setValue(metaSubscriber.getValue());
+				metaList.add(meta);
+			}	
+		}
 
+		return metaList;
+	}
 }
-private List<Meta> convertToMetaList(Collection<Meta> subscriberMetas){
-List<Meta> metaList=new ArrayList<Meta>();
-if(subscriberMetas != null){
-	List<Meta> subscribermetaList=new ArrayList<Meta>(subscriberMetas);
-	for(Meta metaSubscriber:subscribermetaList){
-		Meta meta=new Meta();
-		meta.setKey(metaSubscriber.getKey());
-		meta.setValue(metaSubscriber.getValue());
-		metaList.add(meta);
-	}	
-}
-
-return metaList;
-}}
