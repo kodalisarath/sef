2263157644ffe9eb212e53177603336616abca4d
Sathya Narayanan S <sathya.narayanan.s@ericsson.com>
2014-06-15 01:56:27 +0800

    Missing file.... subscription interface for discovery

---
+++ smart-frontend/src/main/java/com/ericsson/raso/sef/smart/subscription/response/DiscoveryResponse.java
@@ -0,0 +1,31 @@
+package com.ericsson.raso.sef.smart.subscription.response;
+
+import java.util.List;
+
+import com.ericsson.sef.bes.api.entities.Meta;
+import com.ericsson.sef.bes.api.entities.Offer;
+import com.ericsson.sef.bes.api.entities.Product;
+import com.ericsson.sef.bes.api.entities.TransactionStatus;
+
+public class DiscoveryResponse extends AbstractSubscriptionResponse {
+
+	private static final long serialVersionUID = -3985512985259212610L;
+	
+	
+	private TransactionStatus fault;
+	private Offer offer;
+	
+	public TransactionStatus getFault() {
+		return fault;
+	}
+	public void setFault(TransactionStatus fault) {
+		this.fault = fault;
+	}
+	public Offer getOffer() {
+		return offer;
+	}
+	public void setOffer(Offer offer) {
+		this.offer = offer;
+	}
+	
+}
