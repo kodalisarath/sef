e1bf3c1fcc86baff50c2e7240e3bfdb0c19be5ed
chandra_shekhar <chandrashekharpandey@hotmail.com>
2014-06-20 07:01:59 +0800

    ChargingStep class updated for integration with chaging-adapter

--- transaction-engine/src/main/java/com/ericsson/raso/sef/bes/engine/transaction/orchestration/ChargingStep.java
+++ transaction-engine/src/main/java/com/ericsson/raso/sef/bes/engine/transaction/orchestration/ChargingStep.java
@@ -1,18 +1,69 @@
 package com.ericsson.raso.sef.bes.engine.transaction.orchestration;
 
-import com.ericsson.raso.sef.bes.prodcat.tasks.Charging;
+import java.math.BigDecimal;
+import java.util.Map;
+import java.util.Map.Entry;
 
+import org.csapi.schema.parlayx.common.v2_1.ChargingInformation;
+import org.csapi.wsdl.parlayx.payment.amount_charging.v2_1._interface.AmountCharging;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.ericsson.raso.sef.bes.engine.transaction.ServiceResolver;
+import com.ericsson.raso.sef.bes.prodcat.entities.MonetaryUnit;
+import com.ericsson.raso.sef.bes.prodcat.tasks.Charging;
+import com.ericsson.raso.sef.bes.prodcat.tasks.ChargingMode;
 public class ChargingStep extends Step<ChargingStepResult> {
 	private static final long	serialVersionUID	= 6645187522590773212L;
-
+	private static final Logger logger = LoggerFactory.getLogger(ChargingStep.class);
 	ChargingStep(String stepCorrelator, Charging executionInputs) {
 		super(stepCorrelator, executionInputs);
 	}
 
 	@Override
 	public ChargingStepResult execute() {
-		// TODO implement code when there is a client for Charging Adapter available
-		return new ChargingStepResult(null, ((Charging)this.getExecutionInputs()).getCharging());
+		
+		logger.debug("Preparing for Charging");
+		try {
+		MonetaryUnit monetoryUnit = ((Charging)this.getExecutionInputs()).getCharging();
+		ChargingMode mode = ((Charging)this.getExecutionInputs()).getMode();
+		//EndUser Identifier
+		String subscriberId = ((Charging)this.getExecutionInputs()).getSubscriberId();
+		//ChargeAmount
+		String currencyCode = monetoryUnit.getIso4217CurrencyCode();
+		Long amount = monetoryUnit.getAmount();
+		ChargingInformation chargingInformation = new ChargingInformation();
+		chargingInformation.setAmount(BigDecimal.valueOf(amount));
+		chargingInformation.setCode(currencyCode);
+		
+		Map<String, Object> additionalInputs = ((Charging)this.getExecutionInputs()).getAdditionalInputs();
+		logger.debug("additional Inputs: " + additionalInputs.toString());
+		String description = "";
+		if(additionalInputs!=null){
+		for(Entry<String, Object> entry: additionalInputs.entrySet()){
+			description = description+entry.getKey()+";"+entry.getValue()+"|";
+		}
+		description = description.substring(0,description.lastIndexOf("|"));
+		}
+		logger.debug("description is : "+description);
+		chargingInformation.setDescription(description);
+		String referenceCode = stepCorrelator;
+		logger.debug("Charging execution mode: " + mode);
+		AmountCharging request = ServiceResolver.getAmountChargingClient();
+		switch(mode) {
+		case CHARGE:
+				request.chargeAmount(subscriberId, chargingInformation, referenceCode);
+				logger.debug("ChargeStep completed");
+		case REVERSE:
+			//request.chargeAmount(endUserIdentifier, charge, referenceCode);
+			break;
+		}
+		return new ChargingStepResult(null, null);
+	}catch(Exception e){
+		logger.debug("Exception in execution of Charging: Exception: " +  e);
+		ChargingStepResult result = new ChargingStepResult(new StepExecutionException("FulfilmentExecution failed"), null);
+		return result;
+	}
 	}
 
 	@Override
