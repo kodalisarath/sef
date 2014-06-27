a122e9c765e98334ea5c527c62c6a1904fd1dce6
ejosayr <ejosayr@localhost>
2014-06-28 03:21:04 +0800

    Moved to HashMap.  As recommended by Sathya for performance optimization

--- charging-gateway-engine/src/main/java/com/ericsson/raso/sef/smart/subscription/response/RequestCorrelationStore.java
+++ charging-gateway-engine/src/main/java/com/ericsson/raso/sef/smart/subscription/response/RequestCorrelationStore.java
@@ -1,5 +1,6 @@
 package com.ericsson.raso.sef.smart.subscription.response;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.slf4j.Logger;
@@ -11,8 +12,8 @@ import com.ericsson.raso.sef.smart.subscriber.response.HelperConstant;
 public class RequestCorrelationStore {
 	private static final Logger logger = LoggerFactory.getLogger(RequestCorrelationStore.class);
 	//private static Map<String, AbstractSubscriptionResponse> store = new TreeMap<String, AbstractSubscriptionResponse>();
-	private static Map<String, AbstractSubscriptionResponse> store = SefCoreServiceResolver.getCloudAwareCluster().getMap(HelperConstant.SMFE_CORRELATION_STORE);
-	
+	//private static Map<String, AbstractSubscriptionResponse> store = SefCoreServiceResolver.getCloudAwareCluster().getMap(HelperConstant.SMFE_CORRELATION_STORE);
+	private static Map<String, AbstractSubscriptionResponse> store = new HashMap<String, AbstractSubscriptionResponse>();
 	
 	public static void put(String requestId, AbstractSubscriptionResponse response) {
 		store.put(requestId, response);
