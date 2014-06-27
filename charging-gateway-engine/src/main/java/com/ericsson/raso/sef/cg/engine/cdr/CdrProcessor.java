f680f4a3e6abbccd49a9ee5fc9ba96cae821ed29
ejosayr <ejosayr@localhost>
2014-06-28 07:19:26 +0800

    Added Logs to debug - Direct Debit Failure

--- charging-gateway-engine/src/main/java/com/ericsson/raso/sef/cg/engine/cdr/CdrProcessor.java
+++ charging-gateway-engine/src/main/java/com/ericsson/raso/sef/cg/engine/cdr/CdrProcessor.java
@@ -10,12 +10,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.ericsson.raso.sef.core.DateUtil;
+import com.ericsson.raso.sef.core.UniqueIdGenerator;
 import com.ericsson.raso.sef.core.cg.diameter.ChargingInfo;
 import com.ericsson.raso.sef.core.cg.diameter.DiameterUtil;
 
 public class CdrProcessor {
 
-	private Logger log = LoggerFactory.getLogger(CdrProcessor.class);
+	private Logger log = LoggerFactory.getLogger(this.getClass());
 
 	@Handler
 	public void requestIn(Exchange exchange) {
@@ -24,7 +25,11 @@ public class CdrProcessor {
 			ChargingInfo request = (ChargingInfo) exchange.getIn().getBody();
 			Printer printer = new Printer();
 			printer.type = "Request";
-			printer.transactionId = request.getUniqueMessageId();
+			String transactionId = request.getUniqueMessageId();
+			if(transactionId == null)
+				transactionId = UniqueIdGenerator.generateId();
+			exchange.getIn().setHeader("CDR_TransactionId", transactionId);
+			printer.transactionId = transactionId;
 			printer.edrMap = new LinkedHashMap<String, Object>();
 			printer.edrMap.putAll(DiameterUtil.toMap(request.getAvpList()));
 			log.info(printer.toString());
@@ -42,7 +47,7 @@ public class CdrProcessor {
 			ChargingInfo response = (ChargingInfo) exchange.getIn().getBody();
 			Printer printer = new Printer();
 			printer.type = "Response";
-			printer.transactionId = response.getUniqueMessageId();
+			printer.transactionId = (String)exchange.getIn().getHeader("CDR_TransactionId");
 			printer.edrMap = new LinkedHashMap<String, Object>();
 			printer.edrMap.putAll(DiameterUtil.toMap(response.getAvpList()));
 			log.info(printer.toString());
