63ab549c044d27eb721a1457c8c3a549caaa4d35
khaishen-springworks <ks.tan@springworks.com.my>
2014-06-28 06:47:34 +0800

    Add ExternalNotificationEvent converter

---
+++ sef-notification-engine/src/test/java/com/ericsson/sm/core/Meta.java
@@ -0,0 +1,39 @@
+package com.ericsson.sm.core;
+
+import java.io.Serializable;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name="meta")
+public class Meta implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	private String key;
+	private String value;
+
+	public Meta() {}
+
+	public Meta(String key, String value) {
+		this.key = key;
+		this.value = value;
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+}
