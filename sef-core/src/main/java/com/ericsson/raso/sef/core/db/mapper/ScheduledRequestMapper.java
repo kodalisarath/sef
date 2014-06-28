b4b1696ce60a9957b6941a20e95f5c2cfc98f9fc
ejosayr <ejosayr@localhost>
2014-06-29 00:33:09 +0800

    Updated ScheduledRequestMapper.

--- sef-core/src/main/java/com/ericsson/raso/sef/core/db/mapper/ScheduledRequestMapper.java
+++ sef-core/src/main/java/com/ericsson/raso/sef/core/db/mapper/ScheduledRequestMapper.java
@@ -9,22 +9,26 @@ import com.ericsson.raso.sef.core.db.model.ScheduledRequestMeta;
 import com.ericsson.raso.sef.core.db.model.ObsoleteCodeDbSequence;
 
 public interface ScheduledRequestMapper {
-	
+
 	ScheduledRequest getScheduledRequestByJobId(String jobId);
-	
+
 	Collection<ScheduledRequestMeta> getScheduledRequestMetas(long id);
-	
+
 	Collection<ScheduledRequest> findIdenticalRequests(ScheduledRequest request);
-		
+
 	void upadteScheduledRequestStatus(ScheduledRequest request);
-	
+
 	ObsoleteCodeDbSequence scheduledRequestSequence(String rand);
-	
+
 	void insertScheduledRequest(ScheduledRequest request);
-	
+
 	void insertScheduledRequestMeta(ScheduledRequestMeta meta);
-	
-	String getJobId(@Param("msisdn") String msisdn,@Param("requestId") String requestId);
-	
+
+	String getJobId(@Param("msisdn") String msisdn,
+			@Param("requestId") String requestId);
+
+	String getJobIdByOfferId(@Param("msisdn") String msisdn,
+			@Param("offerId") String offerId,
+			@Param("lifeCycleEvent") String lifeCycleEvent);
 
 }
